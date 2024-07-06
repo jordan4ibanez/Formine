d8995928f3a6a59d867ddfbc7e4a907ffa1058cc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-06 05:09:01 -0400

    Wow, that's not good

--- src/raw_c.f90
+++ src/raw_c.f90
@@ -3,14 +3,22 @@ module raw_c
   implicit none
 
   public :: c_free
+  public :: c_strlen
 
   interface
 
     subroutine c_free(ptr) bind(c, name = "free")
       use, intrinsic :: iso_c_binding
       implicit none
-      type(c_ptr), value :: ptr
+      type(c_ptr), intent(in), value :: ptr
     end subroutine c_free
+
+    !! NOTE: This will corrupt the string !!
+    integer(c_int) function c_strlen(ptr) result(length) bind(c, name = "strlen")
+      use, intrinsic :: iso_c_binding
+      implicit none
+      type(c_ptr), intent(in), value :: ptr
+    end function c_strlen
 
   end interface
 end module raw_c
