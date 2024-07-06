04d19b20192b8f5ce27805b1ee851acfdae5dde4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-06 07:34:16 -0400

    And a deallocator for the strings ahhhh

---
+++ src/f_helpers.f90
@@ -0,0 +1,25 @@
+module f_helpers
+  implicit none
+
+  private
+
+  public :: deallocate_string
+
+contains
+
+  ! I have no idea how to use generics yet so good luck to me!
+
+  subroutine deallocate_string(string)
+    implicit none
+    character(:), allocatable :: string
+
+    if (allocated(string)) then
+      print*,"[Deallocating]: "//string
+      deallocate(string)
+    else
+      print*,"[Deallocating]: <String already deallocated>"
+    end if
+
+  end subroutine deallocate_string
+
+end module f_helpers
