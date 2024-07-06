92343c6d3be7cd5cf7abde691efacd01c8d62f38
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-06 09:14:52 -0400

    Now clean up main

--- app/main.f90
+++ app/main.f90
@@ -37,68 +37,50 @@ program main
 
   real :: color = 0.0
 
-
-  ! Try to create a GLFW context.
-  ! if (glfw_init()) then
-  !   print *,"worked"
-  ! else
-  !   print *,"failed"
-  !   return
-  ! end if
-
-  call glfw_get_error()
-
-  ! Try to initialize the Window.
-  ! if (glfw_create_window(640,480, "hi")) then
-  !   print *,"Created window successfully."
-  ! else
-  !   print *,"Failed to create window."
-  !   ! call glfw_terminate()
-  !   return
-  ! end if
-
-  ! call glfw_get_error()
-
   call glfw_set_error_callback()
 
-  ! call glfw_make_context_current()
-
-  ! call glfw_get_error()
-
-
-  ! do while(.not. glfw_window_should_close())
-
-  !   call blah(color)
-
-  !   ! print*,color
-
-  !   call gl_clear_color(color, color, color)
-
-    ! call glfw_get_error()
-
-  !   call gl_clear_color_buffer()
+  ! Try to create a GLFW context.
+  if (glfw_init()) then
+    print *,"worked"
+  else
+    print *,"failed"
+    return
+  end if
 
 
 
-  !   call glfw_swap_buffers()
-
-  !   ! call glfw_get_error()
-
-  !   call glfw_poll_events()
-
-  !   ! call glfw_get_error()
-
-
-  ! end do
+  ! Try to initialize the Window.
+  if (glfw_create_window(640,480, "hi")) then
+    print *,"GLFW: Window created successfully."
+  else
+    print *,"GLFW: Failed to create window."
+    call glfw_terminate()
+    return
+  end if
 
 
+  call glfw_make_context_current()
+
+  do while(.not. glfw_window_should_close())
+
+    call blah(color)
+
+    call gl_clear_color(color, color, color)
+
+    call glfw_get_error()
+
+    call gl_clear_color_buffer()
+
+    call glfw_swap_buffers()
+
+    call glfw_poll_events()
+
+  end do
 
   call glfw_destroy_window()
 
   call glfw_terminate()
 
   call glfw_get_error()
-
-
 
 end
