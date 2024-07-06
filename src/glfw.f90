096af8921a27feb06225f6c25a3474473e5caa82
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-06 11:30:54 -0400

    Okay so NOW we can use a null terminator

--- src/glfw.f90
+++ src/glfw.f90
@@ -40,6 +40,7 @@ module glfw
       integer(c_int), intent(in), value :: width
       integer(c_int), intent(in), value :: height
       character(kind = c_char), intent(in) :: title
+      character(kind = c_char), intent(in), optional :: title
       type(c_ptr), intent(in), optional :: monitor
       type(c_ptr), intent(in), optional :: share
       type(c_ptr) :: new_window_pointer
