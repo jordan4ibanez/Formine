bd5329fd6e4a04c9690ec69dae359b9639386877
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-06 03:48:27 -0400

    Disable everything for more testing

--- app/main.f90
+++ app/main.f90
@@ -39,62 +39,62 @@ program main
 
 
   ! Try to create a GLFW context.
-  if (glfw_init()) then
-    print *,"worked"
-  else
-    print *,"failed"
-    return
-  end if
+  ! if (glfw_init()) then
+  !   print *,"worked"
+  ! else
+  !   print *,"failed"
+  !   return
+  ! end if
 
-  call glfw_get_error()
+  ! call glfw_get_error()
 
   ! Try to initialize the Window.
-  if (glfw_create_window(640,480, "hi")) then
-    print *,"Created window successfully."
-  else
-    print *,"Failed to create window."
-    ! call glfw_terminate()
-    return
-  end if
+  ! if (glfw_create_window(640,480, "hi")) then
+  !   print *,"Created window successfully."
+  ! else
+  !   print *,"Failed to create window."
+  !   ! call glfw_terminate()
+  !   return
+  ! end if
 
-  call glfw_get_error()
+  ! call glfw_get_error()
 
   call glfw_make_context_current()
 
   call glfw_get_error()
 
-  do while(.not. glfw_window_should_close())
+  ! do while(.not. glfw_window_should_close())
 
-    call blah(color)
+  !   call blah(color)
 
-    print*,color
+  !   ! print*,color
 
-    call gl_clear_color(color, color, color)
+  !   call gl_clear_color(color, color, color)
 
-    ! call glfw_get_error()
+  !   ! call glfw_get_error()
 
-    call gl_clear_color_buffer()
-
-
-
-    call glfw_swap_buffers()
-
-    ! call glfw_get_error()
-
-    call glfw_poll_events()
-
-    ! call glfw_get_error()
-
-
-  end do
+  !   call gl_clear_color_buffer()
 
 
 
-  call glfw_destroy_window()
+  !   call glfw_swap_buffers()
 
-  call glfw_get_error()
+  !   ! call glfw_get_error()
 
-  call glfw_terminate()
+  !   call glfw_poll_events()
+
+  !   ! call glfw_get_error()
+
+
+  ! end do
+
+
+
+  ! call glfw_destroy_window()
+
+  ! call glfw_get_error()
+
+  ! call glfw_terminate()
 
 
 
