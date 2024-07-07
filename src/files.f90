3656df5d1c284c67b564d0f8499f3c415e100535
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-07 06:26:47 -0400

    So this is how I think I want to do this

--- src/files.f90
+++ src/files.f90
@@ -1,8 +1,72 @@
+!** To keep file io synchronous, only use this library to work with files.
 module files
   implicit none
 
+
   private
 
+  public :: file_reader
+
+
+  type :: file_reader
+
+    logical :: exists
+    character(len = :), allocatable :: file_string
+
+  contains
+
+    procedure :: read_file => file_reader_read_file
+    procedure :: deallocate => file_reader_deallocate
+
+  end type file_reader
+
+
 contains
+
+
+  !** Open a file, read it into a string, close the file, return the string.
+  !! result%file_string is allocated, remember to call deallocate!
+  subroutine file_reader_read_file(this, file_location)
+    implicit none
+
+    class(file_reader) :: this
+    character(len = *) :: file_location
+    integer :: file_io_identifier
+    integer :: file_size
+
+    ! First we check if the file exists.
+    inquire(file = file_location, exist = this%exists, size = file_size)
+
+    ! If the file does not exist, we're not going to attempt to allocate anything.
+    if (this%exists) then
+
+      ! We want readonly access and streaming of the data into a string.
+      open(newunit = file_io_identifier, file = file_location, status = "old", action = "read", access = "stream")
+
+      ! Now allocate the size of the string.
+      allocate(character(len=file_size) :: this%file_string)
+
+      ! And finally stream it into the string.
+      read(file_io_identifier) this%file_string
+
+    else
+
+      print*,"[files] Error: file_location "//file_location//" does not exist."
+
+    end if
+
+  end subroutine file_reader_read_file
+
+
+  subroutine file_reader_deallocate(this)
+    use deal
+    implicit none
+
+    class(file_reader), intent(in) :: this
+
+    call deallocate_string(this%file_string)
+
+  end subroutine file_reader_deallocate
+
 
 end module files
