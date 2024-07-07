1ef79e52759c956a3a6d2b75793049fe1bb399e5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-07 09:09:18 -0400

    Oh there it is

--- src/shader.f90
+++ src/shader.f90
@@ -3,9 +3,18 @@ module shader
 
   private
 
+  public ::create_shader
+
+  type shader_program
+
+  end type shader_program
+
 contains
 
+  !** Create a named shader program from vertex and fragment code locations
+  !! CAN FAIL. If something blows up or doesn't exist, this will halt the program. (required to render)
   function create_shader(shader_name, vertex_code_location, fragment_code_location) result(success)
+    use opengl
     implicit none
 
     character(len = *) :: shader_name
@@ -13,7 +22,28 @@ contains
     character(len = *) :: fragment_code_location
     logical :: success
 
-    
+    !? Note: needs a 0 check.
+
+    program_id = gl_create_program()
+    print*,"Shader Program ID: "//int_to_string(program_id)
+
+    ! Vertex shader
+    vertex_shader_id = gl_create_shader(GL_VERTEX_SHADER)
+    print*,"Vertex Shader ID: "//int_to_string(vertex_shader_id)
+    call gl_shader_source(vertex_shader_id, vertex_code_location)
+    call gl_compile_shader(vertex_shader_id)
+
+    ! Fragment shader
+    fragment_shader_id = gl_create_shader(GL_FRAGMENT_SHADER)
+    print*,"Fragment Shader ID: "//int_to_string(fragment_shader_id)
+    call gl_shader_source(fragment_shader_id, fragment_code_location)
+    call gl_compile_shader(fragment_shader_id)
+
+    ! Now we attach and link.
+    call gl_attach_shader(program_id, vertex_shader_id)
+    call gl_attach_shader(program_id, fragment_shader_id)
+    call gl_link_program(program_id)
+
 
 
   end function create_shader
