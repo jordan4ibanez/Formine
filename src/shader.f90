514f07c12c5ffe27fc940fe1505dc893175a4775
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-07 09:03:11 -0400

    Now let's package that code into a function

--- src/shader.f90
+++ src/shader.f90
@@ -5,4 +5,17 @@ module shader
 
 contains
 
+  function create_shader(shader_name, vertex_code_location, fragment_code_location) result(success)
+    implicit none
+
+    character(len = *) :: shader_name
+    character(len = *) :: vertex_code_location
+    character(len = *) :: fragment_code_location
+    logical :: success
+
+    
+
+
+  end function create_shader
+
 end module shader
