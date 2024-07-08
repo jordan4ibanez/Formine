0cd66563548aca5ab1b9bc619b1cb3b9df47d0d6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-08 05:56:06 -0400

    Fix bad idea

--- src/shader.f90
+++ src/shader.f90
@@ -16,7 +16,7 @@ contains
 
   !** Create a named shader program from vertex and fragment code locations
   !! CAN FAIL. If something blows up or doesn't exist, this will halt the program. (required to render)
-  function create_shader(shader_name, vertex_code_location, fragment_code_location) result(success)
+  subroutine create_shader(shader_name, vertex_code_location, fragment_code_location)
     use opengl
     use string
     implicit none
@@ -36,17 +36,17 @@ contains
     !? Note: needs a 0 check.
 
     program_id = gl_create_program()
-    print*,"Shader Program ID: "//int_to_string(program_id)
+    print"(A)","Shader Program ID: "//int_to_string(program_id)
 
     ! Vertex shader
     vertex_shader_id = gl_create_shader(GL_VERTEX_SHADER)
-    print*,"Vertex Shader ID: "//int_to_string(vertex_shader_id)
+    print"(A)","Vertex Shader ID: "//int_to_string(vertex_shader_id)
     call gl_shader_source(vertex_shader_id, vertex_code_location)
     call gl_compile_shader(vertex_shader_id)
 
     ! Fragment shader
     fragment_shader_id = gl_create_shader(GL_FRAGMENT_SHADER)
-    print*,"Fragment Shader ID: "//int_to_string(fragment_shader_id)
+    print"(A)","Fragment Shader ID: "//int_to_string(fragment_shader_id)
     call gl_shader_source(fragment_shader_id, fragment_code_location)
     call gl_compile_shader(fragment_shader_id)
 
@@ -55,8 +55,8 @@ contains
     call gl_attach_shader(program_id, fragment_shader_id)
     call gl_link_program(program_id)
 
+    print"(A)","[Shader]: Created shader ["//shader_name//"] successfully."
 
-
-  end function create_shader
+  end subroutine create_shader
 
 end module shader
