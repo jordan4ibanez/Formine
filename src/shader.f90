39d45fa3b1382457e2cea8270302f1e4f59b3ffc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-08 16:59:16 -0400

    Make this very robust

--- src/shader.f90
+++ src/shader.f90
@@ -29,6 +29,22 @@ contains
     success_mutation = success
   end function shader_creation_succeeded
 
+  !** This is a simple variation of shader_creation_succeeded with gl_check_error as our helper.
+  !? Same docs as in shader_creation_success minus the input.
+  function shader_compilation_succeeded(success_mutation) result(success)
+    use opengl
+    use string
+    implicit none
+
+    ! We want to mutate both these variables.
+    logical :: success_mutation
+    logical :: success
+
+    !? 0 means OK in OpenGL.
+    success = gl_get_error() == 0
+    success_mutation = success
+  end function shader_compilation_succeeded
+
   !** Create a named shader program from vertex and fragment code locations
   !! CAN FAIL. If something blows up or doesn't exist, this will halt the program. (required to render)
   function create_shader(shader_name, vertex_code_location, fragment_code_location) result(success)
@@ -67,7 +83,13 @@ contains
     end if
     call gl_shader_source(vertex_shader_id, vertex_code_location)
     call gl_compile_shader(vertex_shader_id)
-    
+    if (.not. shader_compilation_succeeded(success)) then
+      print"(A)","[Shader] Error: Failed to compile vertex for shader ["//shader_name//"]."
+      return
+    else
+      print"(A)","[shader]: Compiled vertex for shader ["//shader_name//"]."
+    end if
+
 
     ! Fragment shader
     fragment_shader_id = gl_create_shader(GL_FRAGMENT_SHADER)
@@ -79,6 +101,12 @@ contains
     end if
     call gl_shader_source(fragment_shader_id, fragment_code_location)
     call gl_compile_shader(fragment_shader_id)
+    if (.not. shader_compilation_succeeded(success)) then
+      print"(A)","[Shader] Error: Failed to compile fragment for shader ["//shader_name//"]."
+      return
+    else
+      print"(A)","[shader]: Compiled fragment for shader ["//shader_name//"]."
+    end if
 
     ! Now we attach and link.
     call gl_attach_shader(program_id, vertex_shader_id)
