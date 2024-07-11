be537b243d65fdd3cdac14980f4851f99787d967
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-11 17:05:28 -0400

    Update crashify.f90

--- src/crashify.f90
+++ src/crashify.f90
@@ -1,22 +1,29 @@
 module crashify
   implicit none
 
+
   private
+
 
   public :: crashy
 
+
 contains
+
 
   logical function oops() result(res)
     implicit none
+
     res = .true.
     ! Try to print while returning to print.
     print*,res
   end function oops
 
+
   subroutine crashy
     ! print in a print frame. Hangs.
     print*,oops()
   end subroutine crashy
+
 
 end module crashify
