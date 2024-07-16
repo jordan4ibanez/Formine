4cd1ff4d14669f6422020f8f3552626c29612d9b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-15 21:51:09 -0400

    Oh my goodness

--- app/main.f90
+++ app/main.f90
@@ -50,9 +50,7 @@ program main
     return
   end if
 
-  call create_attribute_locations([character(len=20) :: "hi", "there"])
-
-  call heap_string_array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26")
+  call create_attribute_locations(heap_string_array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"))
 
   !! This is debugging for functions!
   if (.false.) then
