cadcb9675b04c295ced90078cc9877fe7cc2b32b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-17 04:09:28 -0400

    I write unit tests for fun

--- test/test_heap_string.f90
+++ test/test_heap_string.f90
@@ -12,18 +12,29 @@ contains
   subroutine assign_test()
     implicit none
 
-    type(heap_string) :: blah
+    type(heap_string) :: unit_1
 
-    blah = "hi this is a test"
+    unit_1 = "hi this is a test"
 
-    call assert_str_equal(blah%get(), "hi this is a test")
+    call assert_str_equal(unit_1%get(), "hi this is a test")
   end subroutine assign_test
 
 
   subroutine equality_test()
     implicit none
 
-    
+    type(heap_string) :: unit_1
+    type(heap_string) :: unit_2
+    character(len = :), allocatable :: comparitor_1
+
+    unit_1 = "testing"
+    unit_2 = "not testing"
+
+    comparitor_1 = "testing"
+
+    call assert_str_equal(unit_1%get(), comparitor_1)
+    call assert_str_not_equal(unit_2%get(), comparitor_1)
+    call assert_str_not_equal(unit_1%get(), unit_2%get())
   end subroutine equality_test
 
 
