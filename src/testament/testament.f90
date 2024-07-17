906ef958a7d0e1c3de4876672f5e2a59fda585cb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-17 03:32:47 -0400

    Create testament.f90

---
+++ src/testament/testament.f90
@@ -0,0 +1,26 @@
+!** Testament is a simple testing library I slapped together.
+!? Who's going to write the unit test for the unit test library?
+module testament
+  implicit none
+
+  ! Whole thing is public, purely functional library.
+
+contains
+
+  subroutine assert_str_equal(a,b, error_message)
+    implicit none
+
+    character(len = *), intent(in) :: a
+    character(len = *), intent(in) :: b
+    character(len = *), intent(in), optional :: error_message
+
+    if (a /= b) then
+      if (present(error_message)) then
+        error stop "[Testament] Expected: "//a//" | Received: "//b//" | "//error_message
+      else
+        error stop "[Testament] Expected: "//a//" | received: "//b
+      end if
+    end if
+  end subroutine assert_str_equal
+
+end module testament
