f3ef343028ae480b6755f057972a150b26f5e356
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-16 22:19:34 -0400

    Home grown test suite

--- test/test_heap_string.f90
+++ test/test_heap_string.f90
@@ -1,5 +1,21 @@
-program test_heap_string
+module test_suite
   use string
+  implicit none
+
+  !! Not private.
+
+contains
+
+  subroutine test_1
+    implicit none
+
+  end subroutine test_1
+
+
+end module test_suite
+
+
+program test_heap_string
   implicit none
 
   print*,"hi there"
