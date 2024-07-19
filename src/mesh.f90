df175dbbfdd6fc108e2ec5469f4cb27122141226
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-19 07:57:01 -0400

    Already working, how nice

--- src/mesh.f90
+++ src/mesh.f90
@@ -3,7 +3,21 @@ module mesh
 
   private
 
+  public :: debug_gl_array
+
 contains
+
+  subroutine debug_gl_array
+    use opengl
+    implicit none
+
+    integer :: testing
+
+    testing =  gl_gen_vertex_arrays()
+
+    print*,testing
+
+  end subroutine debug_gl_array
 
 
 
