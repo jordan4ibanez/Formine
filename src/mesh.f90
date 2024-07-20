d252aef77754cd3a9f836df05f0c274ab1ff997d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-20 00:08:52 -0400

    Begin walking the chain

--- src/mesh.f90
+++ src/mesh.f90
@@ -10,9 +10,16 @@ contains
   subroutine debug_create_mesh
     use opengl
     implicit none
+    ! Notes:
+    ! 1. break this up into functions.
+    ! 2. make a type for a mesh.
+    ! 3. make this handle mesh things.
+    ! 4. improve, somehow.
 
     integer :: vao
-    integer :: positions
+    integer :: vbo_positions
+
+    ! Into vertex array object.
 
     vao = gl_gen_vertex_arrays()
 
@@ -20,9 +27,15 @@ contains
 
     call gl_bind_vertex_array(vao)
 
-    positions = gl_gen_buffers()
+    ! Into position vertex buffer object.
 
-    print*,"positions:", positions
+    vbo_positions = gl_gen_buffers()
+
+    print*,"positions:", vbo_positions
+
+    call gl_bind_buffer(GL_ARRAY_BUFFER, vbo_positions)
+
+
 
 
 
