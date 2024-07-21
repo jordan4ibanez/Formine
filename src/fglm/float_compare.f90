9e83a93e1f5bd5d2045fd44cac7e2744fdbc023e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-21 04:46:49 -0400

    Mini speed up

--- src/fglm/float_compare.f90
+++ src/fglm/float_compare.f90
@@ -6,11 +6,11 @@ module float_compare
   public :: f32_is_equal
   public :: f64_is_equal
 
-  ! 9 points of precision, good enough.
-  ! This is stupid anyways.
-  integer, parameter :: precision = 1000000000
+  real, parameter :: tolerence = epsilon(1.0)
+
 
 contains
+
 
   logical function f32_is_equal(a, b) result(equality)
     use, intrinsic :: iso_c_binding
@@ -18,7 +18,7 @@ contains
 
     real(c_float), intent(in), value :: a,b
 
-    equality = floor(abs(a - b) * precision) == 0
+    equality = abs(a - b) <= tolerence
   end function f32_is_equal
 
 
@@ -28,7 +28,7 @@ contains
 
     real(c_double), intent(in), value :: a,b
 
-    equality = (abs(a - b) * precision) == 0
+    equality = abs(a - b) <= tolerence
   end function f64_is_equal
 
 end module float_compare
