3e0230a4e06cea73a083ea08b8a97a61b6bc0b30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-21 04:08:22 -0400

    This is so dumb

--- src/fglm/float_compare.f90
+++ src/fglm/float_compare.f90
@@ -1,6 +1,15 @@
 module float_compare
   implicit none
 
+  private
+
+  public :: f32_is_equal
+  public :: f64_is_equal
+
+  ! 9 points of precision, good enough.
+  ! This is stupid anyways.
+  integer, parameter :: precision = 1000000000
+
 contains
 
   logical function f32_is_equal(a, b) result(equality)
@@ -9,7 +18,7 @@ contains
 
     real(c_float), intent(in), value :: a,b
 
-    equality = abs(a - b) <= epsilon(a)
+    equality = floor(abs(a - b) * precision) == 0
   end function f32_is_equal
 
 
@@ -19,7 +28,7 @@ contains
 
     real(c_double), intent(in), value :: a,b
 
-    equality = abs(a - b) <= epsilon(a)
+    equality = (abs(a - b) * precision) == 0
   end function f64_is_equal
 
 end module float_compare
