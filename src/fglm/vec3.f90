60351ef55cb118b4cd5c0cea62848b6b13973243
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-21 03:12:40 -0400

    Let's make this play nice with C

--- src/fglm/vec3.f90
+++ src/fglm/vec3.f90
@@ -1,12 +1,37 @@
 module vec3
+  use, intrinsic :: iso_c_binding
   implicit none
 
   private
 
   type vec3f
-    real, dimension(3) :: data = 0.0
+    real(c_float), dimension(3) :: data = [0.0, 0.0, 0.0]
   end type vec3f
 
+  interface vec3f
+    module procedure :: constructor_raw,constructor_array
+  end interface
+
 contains
+
+
+  type(vec3f) function constructor_raw(x,y,z) result(new_vec3f)
+    implicit none
+
+    real, intent(in), value :: x,y,z
+
+    new_vec3f%data(1:3) = [x,y,z]
+  end function constructor_raw
+
+
+  type(vec3f) function constructor_array(xyz_array) result(new_vec3f)
+    implicit none
+
+    real, dimension(3), intent(in) :: xyz_array
+
+    new_vec3f%data(1:3) = xyz_array
+  end function constructor_array
+
+
 
 end module vec3
