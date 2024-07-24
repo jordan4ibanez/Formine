4b1c3bd0633384e3c8f57d17d7f2637a8893df73
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-24 09:25:02 -0400

    Update vector_3.f90

--- src/fglm/vector_3.f90
+++ src/fglm/vector_3.f90
@@ -6,7 +6,7 @@ module vector_3f
 
   public :: vec3
 
-  ! vec3 and Vec3 are transparent containers.
+  ! vec3f and Vec3 are transparent containers.
   ! You can use the methods, or you can use the raw data.
   !
   !* They do not mix. Can't add vec3 to vec3, and so forth.
