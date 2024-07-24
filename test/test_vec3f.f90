aa85887d52f9573d24a31ed56498e528bdd43d69
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-24 10:37:02 -0400

    use it some more

--- test/test_vec3f.f90
+++ test/test_vec3f.f90
@@ -17,22 +17,22 @@ contains
     unit_1 = 1.000123
 
 
-    call assert_true(f32_is_equal(unit_1%data(1), 1.000123))
-    call assert_true(f32_is_equal(unit_1%data(2), 1.000123))
-    call assert_true(f32_is_equal(unit_1%data(3), 1.000123))
+    call assert_true(r32_is_equal(unit_1%data(1), 1.000123))
+    call assert_true(r32_is_equal(unit_1%data(2), 1.000123))
+    call assert_true(r32_is_equal(unit_1%data(3), 1.000123))
 
     unit_2 = [1.00101, 1.23456, 1.0003]
 
-    call assert_true(f32_is_equal(unit_2%data(1), 1.00101))
-    call assert_true(f32_is_equal(unit_2%data(2), 1.23456))
-    call assert_true(f32_is_equal(unit_2%data(3), 1.0003))
+    call assert_true(r32_is_equal(unit_2%data(1), 1.00101))
+    call assert_true(r32_is_equal(unit_2%data(2), 1.23456))
+    call assert_true(r32_is_equal(unit_2%data(3), 1.0003))
 
     unit_3 = vec3f(2.87, 9.0001, 10.0)
 
-    call assert_true(f32_is_equal(unit_3%data(1), 2.8700000))
-    call assert_true(f32_is_equal(unit_3%data(2), 9.0001))
+    call assert_true(r32_is_equal(unit_3%data(1), 2.8700000))
+    call assert_true(r32_is_equal(unit_3%data(2), 9.0001))
     ! One step out of precision check.
-    call assert_true(f32_is_equal(unit_3%data(3), 10.0000001))
+    call assert_true(r32_is_equal(unit_3%data(3), 10.0000001))
   end subroutine test_assign
 
 
