75a52b67553e310b8137403c32b1da7718d19fcb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-24 09:25:07 -0400

    Update vector_3.f90

--- src/fglm/vector_3.f90
+++ src/fglm/vector_3.f90
@@ -9,7 +9,7 @@ module vector_3f
   ! vec3f and Vec3 are transparent containers.
   ! You can use the methods, or you can use the raw data.
   !
-  !* They do not mix. Can't add vec3 to vec3, and so forth.
+  !* They do not mix. Can't add vec3f to vec3, and so forth.
 
   type vec3
     real(c_float), dimension(3) :: data = [0.0, 0.0, 0.0]
