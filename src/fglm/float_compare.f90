428e6fac0d6205a1a8ad51e8badc23e0223f47b4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-24 10:04:00 -0400

    Make this more fortranish

--- src/fglm/float_compare.f90
+++ src/fglm/float_compare.f90
@@ -1,4 +1,5 @@
 module float_compare
+  use, intrinsic :: iso_fortran_env, only: real32, real64
   implicit none
 
   private
@@ -7,32 +8,30 @@ module float_compare
   !** f32 has 6 points of precision comparison.
   !** f64 has [test it]
 
-  public :: f32_is_equal
-  public :: f64_is_equal
+  public :: r32_is_equal
+  public :: r64_is_equal
 
-  real, parameter :: tolerence = epsilon(1.0)
+  real, parameter :: tolerence = epsilon(1.0d0)
 
 
 contains
 
 
-  logical function f32_is_equal(a, b) result(equality)
-    use, intrinsic :: iso_c_binding
+  logical function r32_is_equal(a, b) result(equality)
     implicit none
 
-    real(c_float), intent(in), value :: a,b
+    real(real32), intent(in), value :: a,b
 
     equality = abs(a - b) <= tolerence
-  end function f32_is_equal
+  end function r32_is_equal
 
 
-  logical function f64_is_equal(a, b) result(equality)
-    use, intrinsic :: iso_c_binding
+  logical function r64_is_equal(a, b) result(equality)
     implicit none
 
-    real(c_double), intent(in), value :: a,b
+    real(real64), intent(in), value :: a,b
 
     equality = abs(a - b) <= tolerence
-  end function f64_is_equal
+  end function r64_is_equal
 
 end module float_compare
