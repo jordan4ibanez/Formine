bb9c2e1bf7fb9ec2f62c0d756b4de5548f03f687
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-25 13:27:20 -0400

    Perspective is up and running 8)

--- src/camera.f90
+++ src/camera.f90
@@ -13,16 +13,29 @@ contains
 
   subroutine camera_update_matrix()
     use :: glfw, only: glfw_get_aspect_ratio
+    use :: math_stuff, only: to_radians_f32
 
     implicit none
 
     call camera_matrix%identity()
 
-    call camera_matrix%perspective(60.0, glfw_get_aspect_ratio(), 0.1, 100.0)
+    call camera_matrix%perspective(to_radians_f32(70.0), glfw_get_aspect_ratio(), 0.01, 100.0)
 
     ! print*,camera_matrix%data
+
+    call upload_camera_matrix_into_shader()
   end subroutine camera_update_matrix
 
+
+  subroutine upload_camera_matrix_into_shader()
+    use :: opengl
+    use :: shader
+    implicit none
+
+    call gl_uniform_mat4f(shader_get_uniform("main", "camera_matrix"), camera_matrix)
+
+    call gl_uniform_mat4f(shader_get_uniform("main", "object_matrix"), mat4f())
+  end subroutine upload_camera_matrix_into_shader
 
 
 end module camera
