3b8b3aa6c25c6488660d15d3a5c88aed13ac8dd1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-25 13:26:18 -0400

    pi

--- src/constants.f90
+++ src/constants.f90
@@ -20,5 +20,10 @@ module constants
   ! 64 bits. 8 bytes.
   integer(kind = c_int), parameter :: u64_size = sizeof(c_int64_t)
 
+  !** Floating point.
+  ! 32 bits. 4 bytes.
+  real(c_float), parameter :: pi_32 = 3.141592
+  ! 64 bits. 8 bytes.
+  real(c_double), parameter :: pi_64 = 3.141592653589793
 
 end module constants
