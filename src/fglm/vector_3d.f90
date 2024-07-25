3a2052614c0e22c541d260d34eaf9c0a8f4687c2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-25 09:07:43 -0400

    Now organize it properly

--- src/fglm/vector_3d.f90
+++ src/fglm/vector_3d.f90
@@ -70,12 +70,6 @@ contains
     new_vec3d%data(1:3) = [i,i,i]
   end function constructor_scalar_f64
 
-  type(vec3d) function constructor_scalar_f32(i) result(new_vec3d)
-    implicit none
-    real(c_float), intent(in), value :: i
-
-    new_vec3d%data(1:3) = [i,i,i]
-  end function constructor_scalar_f32
 
   type(vec3d) function constructor_raw_f64(x,y,z) result(new_vec3d)
     implicit none
@@ -85,14 +79,6 @@ contains
     new_vec3d%data(1:3) = [x,y,z]
   end function constructor_raw_f64
 
-  type(vec3d) function constructor_raw_f32(x,y,z) result(new_vec3d)
-    implicit none
-
-    real(c_float), intent(in), value :: x,y,z
-
-    new_vec3d%data(1:3) = [x,y,z]
-  end function constructor_raw_f32
-
 
   type(vec3d) function constructor_array_f64(xyz_array) result(new_vec3d)
     implicit none
@@ -101,6 +87,23 @@ contains
 
     new_vec3d%data(1:3) = xyz_array(1:3)
   end function constructor_array_f64
+
+
+  type(vec3d) function constructor_scalar_f32(i) result(new_vec3d)
+    implicit none
+    real(c_float), intent(in), value :: i
+
+    new_vec3d%data(1:3) = [i,i,i]
+  end function constructor_scalar_f32
+
+
+  type(vec3d) function constructor_raw_f32(x,y,z) result(new_vec3d)
+    implicit none
+
+    real(c_float), intent(in), value :: x,y,z
+
+    new_vec3d%data(1:3) = [x,y,z]
+  end function constructor_raw_f32
 
 
   type(vec3d) function constructor_array_f32(xyz_array) result(new_vec3d)
