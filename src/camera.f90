6fac12b0eb13d66ef95e3356c88616c92adcce14
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-25 10:26:03 -0400

    Put the dang camera on the stack for now

---
+++ src/camera.f90
@@ -0,0 +1,22 @@
+module camera
+  use :: matrix_4f
+  implicit none
+
+  private
+
+  !? On the stack, for now. Uses 128 bytes.
+  type(mat4f) :: camera_matrix
+
+contains
+
+  subroutine update_camera_matrix()
+    implicit none
+
+    call camera_matrix%identity()
+
+
+
+  end subroutine update_camera_matrix
+
+
+end module camera
