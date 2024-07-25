ff3ad59ca38440b0caad23c4d56182d8e09d7238
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-25 09:58:47 -0400

    yay

---
+++ test/test_mat4f.f90
@@ -0,0 +1,26 @@
+module test_suite_mat4f
+  use :: matrix_4f
+  implicit none
+
+contains
+
+  subroutine basic_test
+
+    type(mat4f) :: unit_1
+
+    unit_1 = mat4f()
+
+    print*,unit_1
+
+  end subroutine basic_test
+
+end module test_suite_mat4f
+
+
+program test_mat4f
+  use :: test_suite_mat4f
+  implicit none
+
+  call basic_test()
+
+end program test_mat4f
