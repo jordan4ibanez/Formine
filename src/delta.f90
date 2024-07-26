d19edda3e1f1cf69633c1216d6984d5cb2fdb752
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-26 09:12:06 -0400

    Create delta.f90

---
+++ src/delta.f90
@@ -0,0 +1,52 @@
+module delta
+  use, intrinsic :: iso_c_binding, only: c_long, c_double, c_float
+  implicit none
+
+  private
+
+  public :: delta_think
+  public :: get_delta_f64
+  public :: get_delta_f32
+
+  integer(c_long) :: old_delta_integral = 0
+  real(c_double) :: delta_time = 0.0d0
+
+contains
+
+  subroutine delta_think()
+    implicit none
+
+    ! This only works on Windows, Linux, and POSIX platforms.
+    ! If for whatever reason you're porting this, you might want to set this to:
+    ! integer(c_float)
+    ! But doing this will make it so after 24 days, it WILL wrap around!
+    integer(c_long) :: count, count_rate, count_max, new_delta_integral
+
+    call system_clock(count, count_rate, count_max)
+
+    ! Move the calculation into milliseconds
+    new_delta_integral = count / (count_rate / 1000)
+
+    ! Now set it.
+    delta_time = real(new_delta_integral, kind = c_double) - real(old_delta_integral, kind = c_double)
+
+    ! Finally, save it.
+    old_delta_integral = new_delta_integral
+  end subroutine delta_think
+
+
+  real(c_double) function get_delta_f64() result(current_delta)
+    implicit none
+
+    current_delta = delta_time
+  end function get_delta_f64
+
+
+  real(c_float) function get_delta_f32() result(current_delta)
+    implicit none
+
+    ! Basically a precision chop.
+    current_delta = real(delta_time, kind = c_float)
+  end function get_delta_f32
+
+end module delta
