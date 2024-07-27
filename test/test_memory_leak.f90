a56bfa48ee9a9f473e8c0f965c23a9bc68ca4715
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-27 11:34:45 -0400

    We're going to see if these pointers leak

---
+++ test/test_memory_leak.f90
@@ -0,0 +1,12 @@
+module test_suite_test_memory_leak
+  use :: vector_3f
+  implicit none
+
+end module test_suite_test_memory_leak
+
+
+program test_memory_leak
+  use :: test_suite_test_memory_leak
+  implicit none
+
+end program test_memory_leak
