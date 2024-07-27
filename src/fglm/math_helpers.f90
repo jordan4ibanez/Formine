a7e5262e7760caf30a405e1213d49b6dc52fc352
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-27 04:36:35 -0400

    Now make a 64 bit version

--- src/fglm/math_helpers.f90
+++ src/fglm/math_helpers.f90
@@ -51,4 +51,28 @@ contains
     end if
   end function cos_from_sin_f32
 
+
+  !* This was translated from JOML. Original name: "cosFromSinInternal"
+  real(c_double) function cos_from_sin_f64(sin, angle) result(cosine)
+    use :: constants, only: PI_F64, PI_TIMES_2_F64, PI_OVER_2_F64
+    implicit none
+
+    real(c_double), intent(in), value :: sin, angle
+    real(c_double) :: a, b
+
+    cosine = sqrt(1.0 - (sin * sin))
+
+    a = angle + PI_OVER_2_F64
+
+    b = a - idint(a / PI_TIMES_2_F64) * PI_TIMES_2_F64
+
+    if (b < 0.0) then
+      b = PI_TIMES_2_F64 + b
+    end if
+
+    if (b >= PI_F64) then
+      cosine = -cosine
+    end if
+  end function cos_from_sin_f64
+
 end module math_helpers
