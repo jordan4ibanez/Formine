b43d492c75bc200d7804457a15330444a48b67bc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-27 04:30:25 -0400

    And reflect

--- src/fglm/math_helpers.f90
+++ src/fglm/math_helpers.f90
@@ -8,23 +8,23 @@ contains
 
   !* Translate degrees to radians. With 32 bit precision.
   real(c_float) function to_radians_f32(input) result(res)
-    use :: constants, only: PI_32
+    use :: constants, only: PI_F32
     implicit none
 
     real(c_float), intent(in), value :: input
 
-    res = input * (PI_32 / 180.0)
+    res = input * (PI_F32 / 180.0)
   end function to_radians_f32
 
 
   !* Translate degrees to radians. With 64 bit precision.
   real(c_double) function to_radians_f64(input) result(res)
-    use :: constants, only: PI_64
+    use :: constants, only: PI_F64
     implicit none
 
     real(c_double), intent(in), value :: input
 
-    res = input * (PI_64 / 180.0d0)
+    res = input * (PI_F64 / 180.0d0)
   end function to_radians_f64
 
 
