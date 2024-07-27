6f4ba19bc05304233b816f0baf5a7a8b4c5e94bb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-27 04:04:47 -0400

    Fix mess

--- src/fglm/math_helpers.f90
+++ src/fglm/math_helpers.f90
@@ -2,12 +2,14 @@ module math_helpers
   use, intrinsic :: iso_c_binding, only: c_float, c_double
   implicit none
 
+
 contains
 
 
   !* Translate degrees to radians. With 32 bit precision.
   real(c_float) function to_radians_f32(input) result(res)
     use :: constants, only: pi_32
+    implicit none
 
     real(c_float), intent(in), value :: input
 
@@ -18,6 +20,7 @@ contains
   !* Translate degrees to radians. With 64 bit precision.
   real(c_double) function to_radians_f64(input) result(res)
     use :: constants, only: pi_64
+    implicit none
 
     real(c_double), intent(in), value :: input
 
