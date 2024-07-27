fd19c0e5035b2a2dc4667545c9306d26180da3a5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-27 04:27:35 -0400

    constants should probably be capitalized

--- src/constants.f90
+++ src/constants.f90
@@ -5,44 +5,44 @@ module constants
 
   !* Floating point.
   !? 32 bits. 4 bytes.
-  integer(kind = c_int), parameter :: f32_size = sizeof(c_float)
+  integer(kind = c_int), parameter :: F32_SIZE = sizeof(c_float)
   !? 64 bits. 8 bytes.
-  integer(kind = c_int), parameter :: f64_size = sizeof(c_double)
+  integer(kind = c_int), parameter :: F64_SIZE = sizeof(c_double)
 
 
   !* Integral
   !? 32 bits. 4 bytes.
-  integer(kind = c_int), parameter :: i32_size = sizeof(c_int32_t)
+  integer(kind = c_int), parameter :: I32_SIZE = sizeof(c_int32_t)
   !? 64 bits. 8 bytes.
-  integer(kind = c_int), parameter :: i64_size = sizeof(c_int64_t)
+  integer(kind = c_int), parameter :: I64_SIZE = sizeof(c_int64_t)
 
 
   !* Unsigned Integral
   !? 32 bits. 4 bytes.
-  integer(kind = c_int), parameter :: u32_size = sizeof(c_int32_t)
+  integer(kind = c_int), parameter :: U32_SIZE = sizeof(c_int32_t)
   !? 64 bits. 8 bytes.
-  integer(kind = c_int), parameter :: u64_size = sizeof(c_int64_t)
+  integer(kind = c_int), parameter :: U64_SIZE = sizeof(c_int64_t)
 
 
   !* Floating point.
   !? 32 bits. 4 bytes.
-  real(c_float), parameter :: pi_32 = 3.141592
+  real(c_float), parameter :: PI_32 = 3.141592
   !? 64 bits. 8 bytes.
-  real(c_double), parameter :: pi_64 = 3.141592653589793
+  real(c_double), parameter :: PI_64 = 3.141592653589793
 
 
   !* Floating point.
   !? 32 bits. 4 bytes.
-  real(c_float), parameter :: pi_times_2_f32 = pi_32 * 2.0
+  real(c_float), parameter :: PI_TIMES_2_F32 = PI_32 * 2.0
   !? 64 bits. 8 bytes.
-  real(c_double), parameter :: pi_times_2_f64 = pi_64 * 2.0d0
+  real(c_double), parameter :: PI_TIMES_2_F64 = PI_64 * 2.0d0
 
 
   !* Floating point.
   !? 32 bits. 4 bytes.
-  real(c_float), parameter :: pi_over_2_f32 = pi_32 * 0.5
+  real(c_float), parameter :: PI_OVER_2_F32 = PI_32 * 0.5
   !? 64 bits. 8 bytes.
-  real(c_double), parameter :: pi_over_2_f64 = pi_64 * 0.5d0
+  real(c_double), parameter :: PI_OVER_2_F64 = PI_64 * 0.5d0
 
 
 end module constants
