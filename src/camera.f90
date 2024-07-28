c940d5f7864dd627a3724e7dddd50074785c0e3b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-07-28 12:10:44 -0400

    Add the spinning thing from the youtube video

--- src/camera.f90
+++ src/camera.f90
@@ -2,6 +2,7 @@ module camera
   use :: matrix_4f
   use :: vector_3f
   use :: vector_3d
+  use :: math_helpers, only: into_f32
   use, intrinsic :: iso_c_binding, only: c_float, c_double
   implicit none
 
@@ -24,6 +25,10 @@ module camera
   !? Position is not translation, translation is the inverse of position!
   type(vec3d) :: camera_position
   type(vec3d) :: camera_rotation
+
+  !? Object related components.
+  type(mat4f) :: object_matrix
+  real(c_double) :: object_spin
 
 contains
 
@@ -99,13 +104,25 @@ contains
     call camera_matrix%perspective(to_radians_f32(fov_degrees), glfw_get_aspect_ratio(), 0.01, 100.0)
 
 
-    call camera_matrix%rotate_x(camera_rotation%x_f32())
-    call camera_matrix%rotate_y(camera_rotation%y_f32())
+    ! call camera_matrix%rotate_x(camera_rotation%x_f32())
+    ! call camera_matrix%rotate_y(camera_rotation%y_f32())
 
-    call camera_matrix%translate_vec3f(vec3f(camera_position))
+    ! call camera_matrix%translate_vec3f(vec3f(camera_position))
 
     !* So the trick is, the camera actually never moves, but the world moves around it.
     !* This maintains as much precision as possible where you can see it.
+
+
+    !! This hackjob is from the youtube video, remove it!
+    print"(A)", "REMEMBER TO REMOVE YOUTUBE VIDEO THING!"
+
+    object_spin = object_spin + gotten_delta * 1000.0
+    call object_matrix%identity()
+    call object_matrix%translate(into_f32(sin(object_spin / 100.0)), into_f32(cos(object_spin / 314)), -3.0)
+    call object_matrix%rotate_y(to_radians_f32(into_f32(object_spin)))
+    call object_matrix%rotate_x(to_radians_f32(into_f32(object_spin / 16.0)))
+
+
 
     call upload_camera_matrix_into_shader()
   end subroutine camera_update_matrix
@@ -119,7 +136,7 @@ contains
     call gl_uniform_mat4f(shader_get_uniform("main", "camera_matrix"), camera_matrix)
 
     !TODO: REMOVE THIS!
-    call gl_uniform_mat4f(shader_get_uniform("main", "object_matrix"), mat4f())
+    call gl_uniform_mat4f(shader_get_uniform("main", "object_matrix"), object_matrix)
   end subroutine upload_camera_matrix_into_shader
 
 
