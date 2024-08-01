b7a425310558c9c3a79b1f7a40ed350d0c602b0a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-01 06:18:27 -0400

    I ran this for an hour and it only got up to 21 million

--- app/main.f90
+++ app/main.f90
@@ -113,6 +113,10 @@ program main
       call mesh_draw("debug"//long_to_string(debugging))
 
       call mesh_delete("debug"//long_to_string(debugging))
+      
+      if (mod(debugging, 10000) == 0) then
+        print*, debugging
+      end if
 
       ! if (debugging < 2) then
       debugging = debugging + 1
