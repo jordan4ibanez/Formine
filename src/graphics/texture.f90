140bbe5a9b5a0f48a973d024e9ef18efbae86eba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-01 10:29:32 -0400

    And this is our debug

--- src/graphics/texture.f90
+++ src/graphics/texture.f90
@@ -5,7 +5,29 @@ module texture
   private
 
 
+  public :: test_stbi
+
+
 contains
+
+  subroutine test_stbi()
+    use :: stb_image
+    use :: string
+    use :: iso_c_binding
+    implicit none
+
+    integer :: x, y, channels, desired_channels
+    character(len = :, kind = c_char), allocatable :: file_name
+
+    file_name = into_c_string("./textures/rgba_test.png")
+
+    call stbi_load(file_name, x, y, channels, desired_channels)
+
+    print*,x,y,channels,desired_channels
+
+  end subroutine test_stbi
+
+
 
 
 end module texture
