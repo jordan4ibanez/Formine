45a7695981e6f238aa27382ef7714017d2ca2ac6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-01 11:25:25 -0400

    Look at my trash, look at it

--- src/bindings/stb_image.f90
+++ src/bindings/stb_image.f90
@@ -27,14 +27,37 @@ module stb_image
 contains
 
   subroutine stbi_load(file_name, x, y, channels_in_file, desired_channels)
+    use :: math_helpers, only: c_uchar_to_int_array
     implicit none
 
     character(kind = c_char), intent(in) :: file_name
     integer(c_int), intent(inout) :: x, y, channels_in_file
     integer(c_int), intent(in), value :: desired_channels
     type(c_ptr) :: raw_data
+    integer :: array_length
+    integer(1), dimension(:), pointer :: passed_data_pointer
+    integer(1), dimension(:), allocatable :: intermidiate_data_byte
+    integer(c_int), dimension(:), allocatable :: output_data_int
 
     raw_data = internal_stbi_load(file_name, x, y, channels_in_file, desired_channels)
+
+    array_length = x * y * channels_in_file
+
+    call c_f_pointer(raw_data, passed_data_pointer, shape = [array_length])
+
+    print*,passed_data_pointer
+
+    allocate(intermidiate_data_byte(array_length))
+
+    intermidiate_data_byte = passed_data_pointer
+
+    output_data_int = c_uchar_to_int_array(intermidiate_data_byte)
+
+    print*,output_data_int
+
+
+
+
   end subroutine stbi_load
 
 
