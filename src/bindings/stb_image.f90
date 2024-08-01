947d0615db93c7792c96fd6e78f3e20ace38597f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-01 10:29:19 -0400

    Pass by reference

--- src/bindings/stb_image.f90
+++ src/bindings/stb_image.f90
@@ -15,7 +15,7 @@ module stb_image
       use, intrinsic :: iso_c_binding
       implicit none
 
-      character(kind = c_char), intent(in), value :: file_name
+      character(kind = c_char), intent(in) :: file_name
       integer(c_int), intent(inout) :: x, y, channels_in_file
       integer(c_int), intent(in), value :: desired_channels
       type(c_ptr) :: raw_data
@@ -29,13 +29,12 @@ contains
   subroutine stbi_load(file_name, x, y, channels_in_file, desired_channels)
     implicit none
 
-    character(kind = c_char), intent(in), value :: file_name
+    character(kind = c_char), intent(in) :: file_name
     integer(c_int), intent(inout) :: x, y, channels_in_file
     integer(c_int), intent(in), value :: desired_channels
     type(c_ptr) :: raw_data
 
     raw_data = internal_stbi_load(file_name, x, y, channels_in_file, desired_channels)
-    
   end subroutine stbi_load
 
 
