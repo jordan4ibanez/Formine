5929d48a2f0421e3b068f5ef993d736e1270b12c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-02 17:06:36 -0400

    This test is absolutely absurd

--- test/test_byte_conversion.f90
+++ test/test_byte_conversion.f90
@@ -1,4 +1,6 @@
 module test_suite_byte_conversion
+  use, intrinsic :: iso_c_binding
+  use :: testament
   implicit none
 
 contains
@@ -7,15 +9,42 @@ contains
     implicit none
 
     integer(1) :: output
-    integer(4) :: input
+    integer(c_int) :: input
 
     input = 128
 
     output = int(input, kind = 1)
 
     ! print*,output
-
   end subroutine test_it
+
+
+  subroutine now_test_conversions()
+    use :: math_helpers
+    implicit none
+
+    integer(c_int), dimension(256) :: i32
+    integer(1), dimension(256) :: u8
+    integer(c_int), dimension(256) :: conversion_output
+    integer :: i
+
+    ! This is insane overkill.
+    do i = 1,256
+      i32(i) = i - 1
+    end do
+
+    u8 = int_to_c_uchar_array(i32)
+
+    ! If you want to look at this, be my guest.
+    ! print*,u8
+
+    conversion_output = c_uchar_to_int_array(u8)
+
+    do i = 1,256
+      call assert_true(i32(i) == conversion_output(i))
+    end do
+  end subroutine now_test_conversions
+
 
 end module test_suite_byte_conversion
 
@@ -25,4 +54,5 @@ program test_byte_conversion
 
   call test_it()
 
+  call now_test_conversions()
 end program test_byte_conversion
