c45d6bb201a044471ea48c65aafcade1445a60ec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-02 17:09:16 -0400

    Add important information

--- src/graphics/font.f90
+++ src/graphics/font.f90
@@ -12,6 +12,12 @@ module font
   integer, parameter :: character_height = 7
   integer, parameter :: spacing = 1
 
+  integer, parameter :: slot_width = 6
+  integer, parameter :: slot_height = 8
+
+  integer, parameter :: characters_horizontal = 9
+  integer, parameter :: characters_vertical = 9
+
 
 contains
 
