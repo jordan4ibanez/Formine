7fdf73ae66203c08d503dc9bb0d9dd5f520eafb7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-03 08:53:21 -0400

    Push current work

--- src/graphics/font.f90
+++ src/graphics/font.f90
@@ -29,16 +29,39 @@ contains
   subroutine font_prototyping(font_texture_location)
     use :: stb_image
     use :: string
+    use :: files
     implicit none
 
     character(len = *, kind = c_char), intent(in) :: font_texture_location
     integer :: x, y, channels, desired_channels
     character(len = :, kind = c_char), allocatable :: c_file_location
+    character(len = :), allocatable :: font_data_file_name
+    character(len = :), allocatable :: texture_cfg_location
+    type(file_reader) :: reader
+
+    !* We will assume that the only difference in png and the cfg is the file extension.
+
+    font_data_file_name = get_file_name_from_string(font_texture_location)
+
+    print*,font_data_file_name
+
+    texture_cfg_location = string_remove_file_extension(font_texture_location)//".cfg"
+
+    print*,texture_cfg_location
+
+
+
+
+
 
     c_file_location = into_c_string(font_texture_location)
 
     ! We always want 4 channels.
     desired_channels = 4
+
+    ! print*,"    REMEMBER TO USE A SPARE SLOT FOR UNDEFINED CHARACTERS"
+
+
 
 
   end subroutine font_prototyping
