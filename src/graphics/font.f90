a1ba4ee7a51d443d135f6e9eefe883dc483ce00a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-03 07:55:45 -0400

    Wait, I just realized I made the characters black lol

--- src/graphics/font.f90
+++ src/graphics/font.f90
@@ -2,6 +2,9 @@ module font
   use, intrinsic :: iso_c_binding
   implicit none
 
+  !* I am a solo developer. I only use 1 font.
+  !* In the future, if there is a desire for more than one font,
+  !* we will build upon this.
 
   private
 
@@ -12,8 +15,9 @@ module font
   integer, parameter :: character_height = 7
   integer, parameter :: spacing = 1
 
-  integer, parameter :: slot_width = 6
-  integer, parameter :: slot_height = 8
+  ! Slots are the total size of a character, including the border.
+  integer, parameter :: slot_width = character_width + spacing ! 6
+  integer, parameter :: slot_height = character_height + spacing ! 8
 
   integer, parameter :: characters_horizontal = 9
   integer, parameter :: characters_vertical = 9
@@ -22,9 +26,19 @@ module font
 contains
 
 
-  subroutine font_prototyping()
+  subroutine font_prototyping(font_texture_location)
     use :: stb_image
+    use :: string
     implicit none
+
+    character(len = *, kind = c_char), intent(in) :: font_texture_location
+    integer :: x, y, channels, desired_channels
+    character(len = :, kind = c_char), allocatable :: c_file_location
+
+    c_file_location = into_c_string(font_texture_location)
+
+    ! We always want 4 channels.
+    desired_channels = 4
 
 
   end subroutine font_prototyping
