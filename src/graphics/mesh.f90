a42492a98d0bb2b7d788194ec1af2e8d6622b987
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-04 19:57:18 -0400

    Move this into bad ideas

--- src/graphics/mesh.f90
+++ src/graphics/mesh.f90
@@ -123,41 +123,6 @@ contains
   end function upload_positions
 
 
-  integer function upload_positions_vec3f(position_array) result(vbo_position)
-    use, intrinsic :: iso_c_binding
-    use :: opengl
-    use :: shader
-    implicit none
-
-    type(vec3f), dimension(:), intent(in) :: position_array
-    integer :: position_vbo_position
-
-    position_vbo_position = shader_get_attribute("main", "position")
-
-    ! Create the VBO context.
-    vbo_position = gl_gen_buffers()
-
-    if (debug_mode) then
-      print"(A)","vbo position: ["//int_to_string(vbo_position)//"]"
-    end if
-
-    ! Walk into the VBO context.
-    call gl_bind_buffer(GL_ARRAY_BUFFER, vbo_position)
-
-    ! Pass this data into the OpenGL state machine.
-    call gl_buffer_vec3f_array(position_array)
-
-    ! Width = 3 because this is a vec3
-    ! false because this is not normalized
-    ! 0 stride
-    call gl_vertex_attrib_pointer(position_vbo_position, 3, GL_FLOAT, .false., 0)
-
-    ! Enable this new data.
-    call gl_enable_vertex_attrib_array(position_vbo_position)
-
-    ! Now unbind.
-    call gl_bind_buffer(GL_ARRAY_BUFFER, 0)
-  end function upload_positions_vec3f
 
 
   integer function upload_texture_coordinate(texture_coordinate_array) result(vbo_position)
