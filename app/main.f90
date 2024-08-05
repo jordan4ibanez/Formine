af0524cad60ba2a7a37d2ff62b5d61965831302a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-05 17:44:06 -0400

    I think, there is still some performance to be had

--- app/main.f90
+++ app/main.f90
@@ -145,15 +145,15 @@ program main
 
       ! call mesh_draw("debug")
 
-      call camera_set_object_matrix_f32(0.0, -0.25, -3.0, 0.0, rotation, 0.0, 0.5, 0.5, 0.5)
+      ! call camera_set_object_matrix_f32(0.0, -0.25, -3.0, 0.0, rotation, 0.0, 0.5, 0.5, 0.5)
 
-      call texture_use("font")
+      ! call texture_use("font")
 
-      call font_generate_text("hello", 1.0, "Hello, Fortran!", center = .true.)
+      ! call font_generate_text("hello", 1.0, "A", center = .true.)
 
-      call mesh_draw("hello")
+      ! call mesh_draw("hello")
 
-      call mesh_delete("hello")
+      ! call mesh_delete("hello")
 
 
       !? END DRAW TEST ?!
