08a1826d600b9e414cc307abf4123abf701e8ccb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-06 20:22:47 -0400

    We need to create something else from scratch lol

--- src/world/chunk.f90
+++ src/world/chunk.f90
@@ -10,20 +10,26 @@ module chunk
   integer(c_int), parameter :: CHUNK_WIDTH = 16
   integer(c_int), parameter :: CHUNK_HEIGHT = 128
 
+  ! Then we can define it as a flat array for massive caching boost.
+  integer(c_int), parameter :: CHUNK_ARRAY_SIZE = CHUNK_WIDTH * CHUNK_HEIGHT * CHUNK_WIDTH
 
 
+  ! Block data is one element in a chunk.
   type block_data
     ! Starts off as air.
     integer(c_int) :: id = 0
     ! Starts off as pitch black. Range: 0-15
     integer(1) :: light = 0
-    
+    ! There is no use for state yet. So we're going to leave this disabled.
+    ! integer(c_int) :: state = 0
   end type block_data
 
 
+  ! Chunk data is the data for the entire chunk.
   type chunk_data
-
-
+    ! This should automatically be initialized.
+    ! todo: double check just in case.
+    type(block_data), dimension(CHUNK_ARRAY_SIZE) :: data
   end type chunk_data
 
 
