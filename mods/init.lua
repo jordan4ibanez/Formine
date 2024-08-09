11ab3f664b966a2ba8f01236af347f05577e211a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-09 17:18:03 -0400

    create a test global function

--- mods/init.lua
+++ mods/init.lua
@@ -5,3 +5,16 @@ print("hi")
 local x = 5
 
 print(x)
+
+-- The max we want to do with interop is 4 variables.
+function Lua_test(a, b, c, d)
+  print("lua function test")
+  print("a:")
+  print(a)
+  print("b:")
+  print(b)
+  print("c:")
+  print(c)
+  print("d:")
+  print(d)
+end
