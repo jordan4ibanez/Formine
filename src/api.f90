9e1cb16283b6a5b64bf3dfae88a2d4460da1ccfe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-10 05:05:45 -0400

    this is the start of the luajit api

--- src/api.f90
+++ src/api.f90
@@ -1,11 +1,46 @@
 module api
   use :: luajit
+  use, intrinsic :: iso_c_binding
   implicit none
 
 
   private
 
+  public :: api_initialize
+  public :: api_destroy
+
+
+  type(c_ptr) :: lua_state
+
 
 contains
+
+
+  function api_initialize() result(success)
+    implicit none
+
+    logical :: success
+
+    success = .false.
+
+    call luajit_initialize(lua_state)
+
+    success = luajit_run_file(lua_state, "./api/inift.lua")
+
+    ! Someone removed the api init file, eh?
+    if (.not. success) then
+      return
+    end if
+
+
+  end function api_initialize
+
+
+  subroutine api_destroy()
+    implicit none
+
+    call luajit_destroy(lua_state)
+  end subroutine api_destroy
+
 
 end module api
