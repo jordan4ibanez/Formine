225342e2c4ef1fea5de8e83db882a343a830326b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-10 03:04:30 -0400

    lua_isnone

--- src/bindings/luajit.f90
+++ src/bindings/luajit.f90
@@ -379,6 +379,18 @@ contains
   end function lua_isthread
 
 
+  !* Get if a variable is none. This was a macro in LuaJIT.
+  function lua_isnone(state, index) result(is_none)
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    type(c_ptr), intent(in), value :: state
+    integer(c_int), intent(in), value :: index
+    logical(c_bool) :: is_none
+
+    is_none = (lua_type(state, (index)) == LUA_TNONE)
+  end function lua_isnone
+
 
   !* Get the type of a variable as a string.
   function lua_typename(state, index) result(type_name)
