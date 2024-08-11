23139ce9d66b678999a95ce3333e90c76cefeeb3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-11 05:00:36 -0400

    These arrays should be very handy

--- src/array.f90
+++ src/array.f90
@@ -1,19 +1,104 @@
 module array
+  use :: string
   use, intrinsic :: iso_c_binding
   implicit none
 
+
   !* This is just a bunch of different array types.
-  !* They can be used as scalars.
+  !* They can be used as scalars for tricky situations.
 
-  type :: generic_array
-    class(*), dimension(:), allocatable :: data
-  end type generic_array
 
-  type, extends(generic_array) :: int_array
+  type :: int_array
     integer(c_int), dimension(:), allocatable :: data
+  contains
+    generic :: assignment(=) => assign_int
+    procedure :: assign_int
   end type int_array
 
 
+  type :: int64_array
+    integer(c_int64_t), dimension(:), allocatable :: data
+  contains
+    generic :: assignment(=) => assign_int64
+    procedure :: assign_int64
+  end type int64_array
+
+
+  type :: float_array
+    real(c_float), dimension(:), allocatable :: data
+  contains
+    generic :: assignment(=) => assign_float
+    procedure :: assign_float
+  end type float_array
+
+
+  type :: double_array
+    real(c_double), dimension(:), allocatable :: data
+  contains
+    generic :: assignment(=) => assign_double
+    procedure :: assign_double
+  end type double_array
+
+
+  type :: string_array
+    type(heap_string), dimension(:), allocatable :: data
+  contains
+    generic :: assignment(=) => assign_string
+    procedure :: assign_string
+  end type string_array
+
+
+contains
+
+
+  subroutine assign_int(this, new_data)
+    implicit none
+
+    class(int_array), intent(inout) :: this
+    integer(c_int), dimension(:), intent(in) :: new_data
+
+    this%data = new_data
+  end subroutine assign_int
+
+
+  subroutine assign_int64(this, new_data)
+    implicit none
+
+    class(int64_array), intent(inout) :: this
+    integer(c_int64_t), dimension(:), intent(in) :: new_data
+
+    this%data = new_data
+  end subroutine assign_int64
+
+
+  subroutine assign_float(this, new_data)
+    implicit none
+
+    class(float_array), intent(inout) :: this
+    real(c_float), dimension(:), intent(in) :: new_data
+
+    this%data = new_data
+  end subroutine assign_float
+
+
+  subroutine assign_double(this, new_data)
+    implicit none
+
+    class(double_array), intent(inout) :: this
+    real(c_double), dimension(:), intent(in) :: new_data
+
+    this%data = new_data
+  end subroutine assign_double
+
+
+  subroutine assign_string(this, new_data)
+    implicit none
+
+    class(string_array), intent(inout) :: this
+    type(heap_string), dimension(:), intent(in) :: new_data
+
+    this%data = new_data
+  end subroutine assign_string
 
 
 end module array
