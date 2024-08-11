df0f36773dd5d2359d7f4b48429f3c76f1ee4954
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-11 06:10:35 -0400

    I still feel like there is some way to make this better

--- src/array.f90
+++ src/array.f90
@@ -164,86 +164,90 @@ contains
 
     allocate(new_int_array%data(allocation_size))
 
-    index = 0
+    index = 1
 
+    print*,allocation_size
+
+    if (index > allocation_size) return
+    if (present(a)) new_int_array%data(index) = a
+    index = index + 1
+    if (index > allocation_size) then
+      print*,"returning"
+      return
+    end if
+    if (present(b)) new_int_array%data(index) = b
     index = index + 1
     if (index > allocation_size) return
-    if (present(a)) new_int_array%data(1) = a
+    if (present(c)) new_int_array%data(index) = c
     index = index + 1
     if (index > allocation_size) return
-    if (present(b)) new_int_array%data(2) = b
+    if (present(d)) new_int_array%data(index) = d
     index = index + 1
     if (index > allocation_size) return
-    if (present(c)) new_int_array%data(3) = c
+    if (present(e)) new_int_array%data(index) = e
     index = index + 1
     if (index > allocation_size) return
-    if (present(d)) new_int_array%data(4) = d
+    if (present(f)) new_int_array%data(index) = f
     index = index + 1
     if (index > allocation_size) return
-    if (present(e)) new_int_array%data(5) = e
+    if (present(g)) new_int_array%data(index) = g
     index = index + 1
     if (index > allocation_size) return
-    if (present(f)) new_int_array%data(6) = f
+    if (present(h)) new_int_array%data(index) = h
     index = index + 1
     if (index > allocation_size) return
-    if (present(g)) new_int_array%data(7) = g
+    if (present(i)) new_int_array%data(index) = i
     index = index + 1
     if (index > allocation_size) return
-    if (present(h)) new_int_array%data(8) = h
+    if (present(j)) new_int_array%data(index) = j
     index = index + 1
     if (index > allocation_size) return
-    if (present(i)) new_int_array%data(9) = i
+    if (present(k)) new_int_array%data(index) = k
     index = index + 1
     if (index > allocation_size) return
-    if (present(j)) new_int_array%data(10) = j
+    if (present(l)) new_int_array%data(index) = l
     index = index + 1
     if (index > allocation_size) return
-    if (present(k)) new_int_array%data(11) = k
+    if (present(m)) new_int_array%data(index) = m
     index = index + 1
     if (index > allocation_size) return
-    if (present(l)) new_int_array%data(12) = l
+    if (present(n)) new_int_array%data(index) = n
     index = index + 1
     if (index > allocation_size) return
-    if (present(m)) new_int_array%data(13) = m
+    if (present(o)) new_int_array%data(index) = o
     index = index + 1
     if (index > allocation_size) return
-    if (present(n)) new_int_array%data(14) = n
+    if (present(p)) new_int_array%data(index) = p
     index = index + 1
     if (index > allocation_size) return
-    if (present(o)) new_int_array%data(15) = o
+    if (present(q)) new_int_array%data(index) = q
     index = index + 1
     if (index > allocation_size) return
-    if (present(p)) new_int_array%data(16) = p
+    if (present(r)) new_int_array%data(index) = r
     index = index + 1
     if (index > allocation_size) return
-    if (present(q)) new_int_array%data(17) = q
+    if (present(s)) new_int_array%data(index) = s
     index = index + 1
     if (index > allocation_size) return
-    if (present(r)) new_int_array%data(18) = r
+    if (present(t)) new_int_array%data(index) = t
     index = index + 1
     if (index > allocation_size) return
-    if (present(s)) new_int_array%data(19) = s
+    if (present(u)) new_int_array%data(index) = u
     index = index + 1
     if (index > allocation_size) return
-    if (present(t)) new_int_array%data(20) = t
+    if (present(v)) new_int_array%data(index) = v
     index = index + 1
     if (index > allocation_size) return
-    if (present(u)) new_int_array%data(21) = u
+    if (present(w)) new_int_array%data(index) = w
     index = index + 1
     if (index > allocation_size) return
-    if (present(v)) new_int_array%data(22) = v
+    if (present(x)) new_int_array%data(index) = x
     index = index + 1
     if (index > allocation_size) return
-    if (present(w)) new_int_array%data(23) = w
+    if (present(y)) new_int_array%data(index) = y
     index = index + 1
     if (index > allocation_size) return
-    if (present(x)) new_int_array%data(24) = x
-    index = index + 1
-    if (index > allocation_size) return
-    if (present(y)) new_int_array%data(25) = y
-    index = index + 1
-    if (index > allocation_size) return
-    if (present(z)) new_int_array%data(26) = z
+    if (present(z)) new_int_array%data(index) = z
   end function constructor_int
 
 
@@ -252,8 +256,13 @@ contains
 
     integer(c_int64_t), intent(in), optional :: a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
     type(int64_array) :: new_int64_array
+    integer(c_int) :: index, allocation_size
 
-    allocate(new_int64_array%data(ex(a)+ex(b)+ex(c)+ex(d)+ex(e)+ex(f)+ex(g)+ex(h)+ex(i)+ex(j)+ex(k)+ex(l)+ex(m)+ex(n)+ex(o)+ex(p)+ex(q)+ex(r)+ex(s)+ex(t)+ex(u)+ex(v)+ex(w)+ex(x)+ex(y)+ex(z)))
+    allocation_size = ex(a)+ex(b)+ex(c)+ex(d)+ex(e)+ex(f)+ex(g)+ex(h)+ex(i)+ex(j)+ex(k)+ex(l)+ex(m)+ex(n)+ex(o)+ex(p)+ex(q)+ex(r)+ex(s)+ex(t)+ex(u)+ex(v)+ex(w)+ex(x)+ex(y)+ex(z)
+
+    allocate(new_int64_array%data(allocation_size))
+
+    index = 0
 
     if (present(a)) new_int64_array%data(1) = a
     if (present(b)) new_int64_array%data(2) = b
