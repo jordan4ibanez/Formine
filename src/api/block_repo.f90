1eaa722acf0cf7f7b77a01e5906e9fc617d201df
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-10 20:02:12 -0400

    Clear out this debugging

--- src/api/block_repo.f90
+++ src/api/block_repo.f90
@@ -19,6 +19,7 @@ contains
     type(c_ptr), intent(in), value :: state
     logical :: success
 
+
     ! Memory layout: (Stack grows down.)
     ! -1 - blocks = {}
     ! then moves to:
@@ -40,25 +41,8 @@ contains
     call luajit_swap_table_function(state, "register_block", c_funloc(register_block))
 
 
-    ! Now clear the stack.
+    ! Now clear the stack. We're done with the block LuaJIT table.
     call lua_pop(state, lua_gettop(state))
-
-    ! ! Let's try to call it.
-    ! call lua_pushstring(state, "register_block")
-    ! ! Table is now at -2.
-    ! call lua_gettable(state, -2)
-
-    ! if (lua_pcall(state, 0, 0, 0) == LUA_OK) then
-    !   print*,"it worked"
-    ! end if
-
-    ! The pcall has removed the function off the stack.
-    ! The table is now back at -1.
-    ! if (.not. lua_istable(state, -1)) then
-    !   print"(A)", "this is no longer a table"
-    ! else
-    !   print"(A)", "that's still a table"
-    ! end if
   end function block_repo_deploy_lua_api
 
 
