cac9dbc6dec5cf9680d3f443b0b1ee2ab8236f30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-11 06:00:14 -0400

    Squish it

--- src/array.f90
+++ src/array.f90
@@ -164,7 +164,8 @@ contains
 
     allocate(new_int_array%data(allocation_size))
 
-    new_int_array%data(1:allocation_size) = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+    ! Truncation is very handy.
+    new_int_array%data(1:3) = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
   end function constructor_int
 
 
@@ -176,84 +177,32 @@ contains
 
     allocate(new_int64_array%data(ex(a)+ex(b)+ex(c)+ex(d)+ex(e)+ex(f)+ex(g)+ex(h)+ex(i)+ex(j)+ex(k)+ex(l)+ex(m)+ex(n)+ex(o)+ex(p)+ex(q)+ex(r)+ex(s)+ex(t)+ex(u)+ex(v)+ex(w)+ex(x)+ex(y)+ex(z)))
 
-    if (present(a)) then
-      new_int64_array%data(1) = a
-    end if
-    if (present(b)) then
-      new_int64_array%data(2) = b
-    end if
-    if (present(c)) then
-      new_int64_array%data(3) = c
-    end if
-    if (present(d)) then
-      new_int64_array%data(4) = d
-    end if
-    if (present(e)) then
-      new_int64_array%data(5) = e
-    end if
-    if (present(f)) then
-      new_int64_array%data(6) = f
-    end if
-    if (present(g)) then
-      new_int64_array%data(7) = g
-    end if
-    if (present(h)) then
-      new_int64_array%data(8) = h
-    end if
-    if (present(i)) then
-      new_int64_array%data(9) = i
-    end if
-    if (present(j)) then
-      new_int64_array%data(10) = j
-    end if
-    if (present(k)) then
-      new_int64_array%data(11) = k
-    end if
-    if (present(l)) then
-      new_int64_array%data(12) = l
-    end if
-    if (present(m)) then
-      new_int64_array%data(13) = m
-    end if
-    if (present(n)) then
-      new_int64_array%data(14) = n
-    end if
-    if (present(o)) then
-      new_int64_array%data(15) = o
-    end if
-    if (present(p)) then
-      new_int64_array%data(16) = p
-    end if
-    if (present(q)) then
-      new_int64_array%data(17) = q
-    end if
-    if (present(r)) then
-      new_int64_array%data(18) = r
-    end if
-    if (present(s)) then
-      new_int64_array%data(19) = s
-    end if
-    if (present(t)) then
-      new_int64_array%data(20) = t
-    end if
-    if (present(u)) then
-      new_int64_array%data(21) = u
-    end if
-    if (present(v)) then
-      new_int64_array%data(22) = v
-    end if
-    if (present(w)) then
-      new_int64_array%data(23) = w
-    end if
-    if (present(x)) then
-      new_int64_array%data(24) = x
-    end if
-    if (present(y)) then
-      new_int64_array%data(25) = y
-    end if
-    if (present(z)) then
-      new_int64_array%data(26) = z
-    end if
+    if (present(a)) new_int64_array%data(1) = a
+    if (present(b)) new_int64_array%data(2) = b
+    if (present(c)) new_int64_array%data(3) = c
+    if (present(d)) new_int64_array%data(4) = d
+    if (present(e)) new_int64_array%data(5) = e
+    if (present(f)) new_int64_array%data(6) = f
+    if (present(g)) new_int64_array%data(7) = g
+    if (present(h)) new_int64_array%data(8) = h
+    if (present(i)) new_int64_array%data(9) = i
+    if (present(j)) new_int64_array%data(10) = j
+    if (present(k)) new_int64_array%data(11) = k
+    if (present(l)) new_int64_array%data(12) = l
+    if (present(m)) new_int64_array%data(13) = m
+    if (present(n)) new_int64_array%data(14) = n
+    if (present(o)) new_int64_array%data(15) = o
+    if (present(p)) new_int64_array%data(16) = p
+    if (present(q)) new_int64_array%data(17) = q
+    if (present(r)) new_int64_array%data(18) = r
+    if (present(s)) new_int64_array%data(19) = s
+    if (present(t)) new_int64_array%data(20) = t
+    if (present(u)) new_int64_array%data(21) = u
+    if (present(v)) new_int64_array%data(22) = v
+    if (present(w)) new_int64_array%data(23) = w
+    if (present(x)) new_int64_array%data(24) = x
+    if (present(y)) new_int64_array%data(25) = y
+    if (present(z)) new_int64_array%data(26) = z
   end function constructor_int64
 
 
