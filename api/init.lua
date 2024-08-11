270be8ae5db585a9c00c09e585a725aacd1f4777
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-10 20:00:32 -0400

    Fix docs

--- api/init.lua
+++ api/init.lua
@@ -8,10 +8,12 @@ This is written like this, so you can see the data using the Lua VSCode extensio
 
 print("[LuAJIT API]: Initializing.")
 
-blocks = {
+--- The block library.
+--- You use this to set and get block definitions.
+block = {
   --- Register a block into the engine.
-  ---@param name string The name of the block.
-  ---@param data_table table A table containing block definition data.
+  --- @param name string The name of the block.
+  --- @param data_table table A table containing block definition data.
   register_block = function(name, data_table) print("test") end,
   test = 5.5
 }
