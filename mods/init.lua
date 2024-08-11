42cf0bb6255357918c80582a363c2a5dd6341bc4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-11 00:48:52 -0400

    Add my debugging thing

--- mods/init.lua
+++ mods/init.lua
@@ -3,4 +3,5 @@ block.register({
   description = "Air",
   textures = { "air.png", "air.png", "air.png", "air.png", "air.png", "air.png" },
   draw_type = block.draw_type.air,
+  debugging = "I am from luajit"
 })
