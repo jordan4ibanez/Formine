69e82fb69807f0a3f0118a06803d872a401c1b90
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-12 22:17:27 -0400

    I had to think about this for a long time

--- mods/init.lua
+++ mods/init.lua
@@ -1,6 +1,6 @@
 block.register({
-  name = "air",
-  description = "Air",
-  textures = { "air.png", "air.png", "air.png", "air.png", "air.png", "air.png" },
-  draw_type = block.draw_type.air,
+  name = "stone",
+  description = "Stone",
+  textures = { "stone.png", "stone.png", "stone.png", "stone.png", "stone.png", "stone.png" },
+  draw_type = block.draw_type.normal,
 })
