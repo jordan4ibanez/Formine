72efb1704dc5fd8960b9f2e22b94e89623dabbec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-13 09:35:59 -0400

    Clean up chunk, next step, textures

--- src/world/chunk.f90
+++ src/world/chunk.f90
@@ -1,4 +1,5 @@
 module chunk
+  use :: string
   use, intrinsic :: iso_c_binding
   implicit none
 
@@ -6,15 +7,24 @@ module chunk
   private
 
 
-  ! Width stands for X and Z. There is no sense in defining depth as they're equal sized.
+  !* Width stands for X and Z. There is no sense in defining depth as they're equal sized.
+
   integer(c_int), parameter :: CHUNK_WIDTH = 16
   integer(c_int), parameter :: CHUNK_HEIGHT = 128
 
-  ! Then we can define it as a flat array for massive caching boost.
+  !* Then we can define it as a flat array for massive caching boost.
+
   integer(c_int), parameter :: CHUNK_ARRAY_SIZE = CHUNK_WIDTH * CHUNK_HEIGHT * CHUNK_WIDTH
 
+  !* The chunk is divided up into 8 meshes with width, height, and depth of 16.
+  !* 1 is the bottom, 8 is the top.
 
-  ! Block data is one element in a chunk.
+  integer(c_int), parameter :: MESH_STACK_ARRAY_SIZE = 8
+  integer(c_int), parameter :: MESH_STACK_HEIGHT = 16
+
+
+  !* Block data is one element in a chunk.
+
   type block_data
     ! Starts off as air.
     integer(c_int) :: id = 0
@@ -25,12 +35,14 @@ module chunk
   end type block_data
 
 
-  ! Chunk data is the data for the entire chunk.
+  !* Chunk data is the data for the entire chunk.
+
   type chunk_data
-    ! This should automatically be initialized.
-    ! todo: double check just in case.
     type(block_data), dimension(CHUNK_ARRAY_SIZE) :: data
+    type(heap_string), dimension(MESH_STACK_ARRAY_SIZE) :: mesh
   end type chunk_data
+
+
 
 
 contains
