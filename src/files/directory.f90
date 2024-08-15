07c770c2919f64d977d488d61b387140d1cda33a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-15 11:09:55 -0400

    I literally cannot believe this works on posix

--- src/files/directory.f90
+++ src/files/directory.f90
@@ -24,6 +24,7 @@ module directory
   !* Ultra duct-taped together directory reader.
   !* We already have the tools on the system, use them.
   type :: directory_reader
+    type(heap_string), dimension(:), allocatable :: files
   contains
     procedure :: read_directory
   end type directory_reader
@@ -39,7 +40,7 @@ contains
     logical :: pipe_exists
     character(len = *, kind = c_char), intent(in) :: path
     character(len = :, kind = c_char), allocatable :: message
-    integer :: status
+    integer :: file_count, status, file_io_identifier, i, message_length
 
     ! We are literally creating a custom pipe on posix to act as a virtual file.
     !! DO NOT use this for high performance IO. I have no idea how fast this actually is lol.
@@ -71,15 +72,40 @@ contains
     end if
 
 
+    ! Open up this file reader so we can trigger bash to start dumping in raw data.
+    open(file = FIFO_PIPE, newunit = file_io_identifier, access = "sequential", action = "readwrite")
+
+    ! We're going to use some hackjobery to get the number of files before hand.
+    call execute_command_line("ls -1 "//path//"| wc -l >"//FIFO_PIPE, wait = .true.)
+    read(file_io_identifier, "(i8)") file_count
+
+    ! Nothing to do.
+    if (file_count == 0) then
+      print*,"bailing out."
+      open(unit = 7023, iostat = status, file = FIFO_PIPE, status = "old")
+      close(7023, status = "delete")
+    end if
+
+    ! Now we will overwrite this thing and loop for X amount of file names.
     call execute_command_line("ls -1 "//path//" > "//FIFO_PIPE, wait = .true.)
 
+    ! This should probably be 4096 since that's the linux limit.
+    allocate(character(256) :: message)
 
+    ! Finally, allocate self with X amount of file names.
+    allocate(this%files(file_count))
 
-    ! call get_command(command = message)
+    do i = 1,file_count
+      read(file_io_identifier, "(A)") message
+      message_length = index(message, " ") - 1
+      this%files(i) = message(1:message_length)
+    end do
 
-    ! wait(id = id_var)
+    ! Finally, delete the FIFO pipe.
+    close(file_io_identifier, status = "delete")
 
-    ! print*,message
+    print*,"[", this%files, "]"
+
   end subroutine read_directory
 
 
