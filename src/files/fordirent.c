7ee47041f66f6000204c0b69eaad42be0d5793d5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-16 07:55:01 -0400

    We need to make this fortran capable

--- src/files/fordirent.c
+++ src/files/fordirent.c
@@ -1,4 +1,20 @@
-#include <dirent.h> 
-#include <stdio.h> 
+#include <dirent.h>
+#include <stdio.h>
 
-// I bet you didn't expect C in this project!
+// You can thank:
+// Jean-Bernard Jansen https://stackoverflow.com/a/4204758
+void parse_directory_folders()
+{
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(".");
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
+  return (0);
+}
