8be5d195165943d89eaa0a978253625aca47f95a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-18 09:04:41 -0400

    Push all this

--- src/graphics/texture_packer/packer/texture_packer_skyline_packer.f90
+++ src/graphics/texture_packer/packer/texture_packer_skyline_packer.f90
@@ -1,7 +1,53 @@
 module texture_packer_skyline_packer
   use :: texture_packer_frame
   use :: texture_packer_rectangle
+  use :: texture_packer_config
   use, intrinsic :: iso_c_binding
   implicit none
+
+  !* This is not wise to make this one module, but I will mimic the Rust impl.
+  !* Nothing is documented, so I will mirror that.
+
+  type :: skyline
+    integer(c_int) :: x = 0
+    integer(c_int) :: y = 0
+    integer(c_int) :: w = 0
+  contains
+    procedure :: left => skyline_left
+    procedure :: right => skyline_right
+  end type skyline
+
+
+  type :: skyline_packer
+    type(texture_packer_conf) :: config
+    type(rect) :: border
+    type(skyline), dimension(:), allocatable :: skylines
+  end type skyline_packer
+
+
+contains
+
+
+!* SKYLINE. =================================================================================
+
+
+  function skyline_left(this) result(left)
+    implicit none
+
+    class(skyline), intent(in) :: this
+    integer(c_int) :: left
+
+    left = this%x
+  end function skyline_left
+
+
+  function skyline_right(this) result(right)
+    implicit none
+
+    class(skyline), intent(in) :: this
+    integer(c_int) :: right
+
+    right = this%x + this%w - 1
+  end function skyline_right
 
 end module texture_packer_skyline_packer
