39b3f4fcbf16ec1930e5634c8d73d30d668109a6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-18 08:55:00 -0400

    translate texture_packer_config.rs

---
+++ src/graphics/texture_packer/packer/texture_packer_config.f90
@@ -0,0 +1,40 @@
+module texture_packer_config
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+
+  type :: texture_packer_conf
+    !*
+    !* layout configuration
+    !*
+    !* Max width of the packed image. Default value is `1024`.
+    integer(c_int) :: max_width = 1024
+    !* Max height of the packed image. Default value is `1024`.
+    integer(c_int) :: max_height = 1024
+    !* True to allow rotation of the input images. Default value is `false`. Images rotated will be
+    !* rotated 90 degrees clockwise.
+    logical(c_bool) :: allow_rotation = .false.
+
+    !* If enabled, the size of the output texture will always match [max_width] and [max_height]
+    !* leaving potentially much unused space on the texture.
+    logical(c_bool) :: force_max_dimensions = .false.
+
+    !*
+    !* texture configuration
+    !*
+    !* Size of the padding on the outer edge of the packed image in pixel. Default value is `0`.
+    integer(c_int) :: border_padding = 0
+    !* Size of the padding between frames in pixel. Default value is `2`
+    integer(c_int) :: texture_padding = 2
+    !* Size of the repeated pixels at the border of each image. Default value is `0`.
+    integer(c_int) :: texture_extrusion = 0
+
+    !* True to trim the empty pixels of the input images. Default value is `true`.
+    logical(c_bool) :: trim = .true.
+
+    !* True to draw the red line on the edge of the each frames. Useful for debugging. Default
+    !* value is `false`.
+    logical(c_bool) :: texture_outlines = .false.
+  end type texture_packer_conf
+
+end module texture_packer_config
