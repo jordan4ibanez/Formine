c6365a908a98e71fc6e404af5d2ed7ed658e9f39
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-18 04:57:08 -0400

    A bunch of unit testing

--- test/test_general_string_prototyping.f90
+++ test/test_general_string_prototyping.f90
@@ -12,7 +12,7 @@ contains
 
     character(len = :, kind = c_char), allocatable :: unit_1, unit_2, unit_3
 
-    print*, "BEGIN TESTING STRING STARTS WITH."
+    ! print*, "BEGIN TESTING STRING STARTS WITH."
 
     unit_1 = "hi there"
 
@@ -48,7 +48,7 @@ contains
 
     character(len = :, kind = c_char), allocatable :: unit_1, unit_2, unit_3
 
-    print*, "BEGIN TESTING STRING ENDS WITH."
+    ! print*, "BEGIN TESTING STRING ENDS WITH."
 
     unit_1 = "hi there"
 
@@ -80,6 +80,52 @@ contains
   end subroutine ends_with
 
 
+  subroutine cut_first()
+    implicit none
+
+    character(len = :, kind = c_char), allocatable :: unit_1, unit_2, unit_3, unit_4, unit_5, unit_6
+
+    unit_1 = "hi hi hi"
+
+    call assert_str_equal(string_cut_first(unit_1, "hi "), "hi hi")
+
+    ! This module is not allowed to cut blank data.
+    unit_2 = " "
+
+    call assert_str_equal(string_cut_first(unit_2, " "), " ")
+
+    unit_3 = "cooltest"
+
+    call assert_str_equal(string_cut_first(unit_3, "cool"), "test")
+    call assert_str_equal(string_cut_first(unit_3, "test"), "cool")
+    call assert_str_equal(string_cut_first(unit_3, "stco"), "cooltest")
+    call assert_str_equal(string_cut_first(unit_3, "olte"), "cost")
+    call assert_str_equal(string_cut_first(unit_3, "t"), "coolest")
+
+    unit_4 = "the quick brown fox jumps over the lazy dog"
+
+    call assert_str_equal(string_cut_first(unit_4, "the"), " quick brown fox jumps over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "quick"), "the  brown fox jumps over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "brown"), "the quick  fox jumps over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "fox"), "the quick brown  jumps over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "jumps"), "the quick brown fox  over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "over"), "the quick brown fox jumps  the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "the"), " quick brown fox jumps over the lazy dog")
+    call assert_str_equal(string_cut_first(unit_4, "lazy"), "the quick brown fox jumps over the  dog")
+    call assert_str_equal(string_cut_first(unit_4, "dog"), "the quick brown fox jumps over the lazy ")
+    call assert_str_equal(string_cut_first(unit_4, " "), "thequick brown fox jumps over the lazy dog")
+
+    unit_5 = "hello there"
+
+    call assert_str_equal(string_cut_first(unit_5, " "), "hellothere")
+
+    unit_6 = "a"
+
+    call assert_str_equal(string_cut_first(unit_6, "a"), "")
+
+  end subroutine cut_first
+
+
 end module test_suite_string_prototyping
 
 program test_string_prototyping
@@ -90,4 +136,5 @@ program test_string_prototyping
 
   call ends_with()
 
+  call cut_first()
 end program test_string_prototyping
