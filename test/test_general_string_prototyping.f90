ec8846e5f32fd90d8c28d3e649315e2be9da878a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-18 03:17:03 -0400

    Unit test, literally everything

--- test/test_general_string_prototyping.f90
+++ test/test_general_string_prototyping.f90
@@ -10,9 +10,9 @@ contains
   subroutine starts_with()
     implicit none
 
-    character(len = :, kind = c_char), allocatable :: unit_1, unit_2
+    character(len = :, kind = c_char), allocatable :: unit_1, unit_2, unit_3
 
-    print*, "BEGIN TESTING STARTS WITH."
+    print*, "BEGIN TESTING STRING STARTS WITH."
 
     unit_1 = "hi there"
 
@@ -20,6 +20,8 @@ contains
     call assert_false(string_starts_with(unit_1, "i"))
     call assert_true(string_starts_with(unit_1, "hi "))
     call assert_true(string_starts_with(unit_1, "hi t"))
+    call assert_false(string_starts_with(unit_1, ""))
+    call assert_false(string_starts_with(unit_1, " "))
 
     unit_2 = "[test = 1]"
 
@@ -27,13 +29,54 @@ contains
     call assert_false(string_starts_with(unit_2, "[t "))
     call assert_true(string_starts_with(unit_2, "[tes"))
     call assert_true(string_starts_with(unit_2, "[test"))
+    call assert_false(string_starts_with(unit_2, ""))
+    call assert_false(string_starts_with(unit_2, " "))
+
+    unit_3 = ""
+
+    call assert_false(string_starts_with(unit_3, "["))
+    call assert_false(string_starts_with(unit_3, "[t "))
+    call assert_false(string_starts_with(unit_3, "[tes"))
+    call assert_false(string_starts_with(unit_3, "[test"))
+    call assert_false(string_starts_with(unit_3, ""))
+    call assert_false(string_starts_with(unit_3, " "))
   end subroutine starts_with
 
 
   subroutine ends_with()
+    implicit none
 
+    character(len = :, kind = c_char), allocatable :: unit_1, unit_2, unit_3
 
+    print*, "BEGIN TESTING STRING ENDS WITH."
 
+    unit_1 = "hi there"
+
+    call assert_true(string_ends_with(unit_1, "there"))
+    call assert_false(string_ends_with(unit_1, "er"))
+    call assert_true(string_ends_with(unit_1, " there"))
+    call assert_true(string_ends_with(unit_1, "here"))
+    call assert_false(string_ends_with(unit_1, "1"))
+    call assert_false(string_ends_with(unit_1, ""))
+    call assert_false(string_ends_with(unit_1, " "))
+
+    unit_2 = "[test = 1]"
+
+    call assert_true(string_ends_with(unit_2, "]"))
+    call assert_false(string_ends_with(unit_2, "1] "))
+    call assert_true(string_ends_with(unit_2, " 1]"))
+    call assert_true(string_ends_with(unit_2, " = 1]"))
+    call assert_false(string_ends_with(unit_2, ""))
+    call assert_false(string_ends_with(unit_2, " "))
+
+    unit_3 = ""
+
+    call assert_false(string_starts_with(unit_3, "["))
+    call assert_false(string_starts_with(unit_3, "[t "))
+    call assert_false(string_starts_with(unit_3, "[tes"))
+    call assert_false(string_starts_with(unit_3, "[test"))
+    call assert_false(string_starts_with(unit_3, ""))
+    call assert_false(string_starts_with(unit_3, " "))
   end subroutine ends_with
 
 
