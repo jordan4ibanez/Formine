73bcf6bea50069404cb615fb6bc17527601d0691
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-21 12:00:18 -0400

    Time to set up this mess for debugging lol

--- test/test_rust_texture_packer_translation.f90
+++ test/test_rust_texture_packer_translation.f90
@@ -7,14 +7,29 @@ contains
 
 
   subroutine begin_test()
+    use :: iso_c_binding
+    use :: string
     implicit none
 
     type(texture_packer_conf) :: config
     type(texture_packer) :: packer
+    integer :: i
+    character(len = :, kind = c_char), allocatable :: root_path, temp_path
 
     config%trim = .false.
 
     packer = texture_packer(config)
+
+
+    root_path = "./test/textures/"
+
+
+    do i = 1,11
+
+      temp_path = root_path//int_to_string(i)//".png"
+      print*,temp_path
+
+    end do
 
   end subroutine
 
