9b9fd795e0a16af738c4c4e51d432fa7ac57335b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-21 12:16:13 -0400

    it can pack 1 image so far

--- test/test_rust_texture_packer_translation.f90
+++ test/test_rust_texture_packer_translation.f90
@@ -9,25 +9,41 @@ contains
   subroutine begin_test()
     use :: iso_c_binding
     use :: string
+    use :: stb_image
+    use :: rgba8_texture_module
     implicit none
 
     type(texture_packer_conf) :: config
     type(texture_packer) :: packer
     integer :: i
     character(len = :, kind = c_char), allocatable :: root_path, temp_path
+    integer(1), dimension(:), allocatable :: raw_image_data
+    integer(c_int) :: width, height, channels
+    type(rgba8_texture) :: rgba_image_data
+
 
     config%trim = .false.
 
     packer = texture_packer(config)
 
-
     root_path = "./test/textures/"
 
-
-    do i = 1,11
+    do i = 1,1
 
       temp_path = root_path//int_to_string(i)//".png"
       print*,temp_path
+
+      raw_image_data = stbi_load(temp_path, width, height, channels, 4)
+
+      print*,"len",size(raw_image_data) / 4
+      print*,width, height, channels
+
+      rgba_image_data = rgba8_texture(raw_image_data, width, height)
+
+
+      if(packer%pack_own(temp_path, rgba_image_data) == TEXTURE_PACKER_OK) then
+
+      end if
 
     end do
 
