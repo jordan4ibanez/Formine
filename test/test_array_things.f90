f5c3e45286c105c265066a622576d93058c7ecf3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-22 10:59:47 -0400

    Create test_array_things.f90

---
+++ test/test_array_things.f90
@@ -0,0 +1,30 @@
+module test_array_things_test_suite
+  use :: array, only: array_i32_unique
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+contains
+
+  subroutine testing()
+    implicit none
+
+    integer(c_int), dimension(:), allocatable :: unit_1, unit_2
+
+    unit_1 = (/1,2,3,4,5,5,1,2,7,42,5,1,2/)
+
+    unit_2 = array_i32_unique(unit_1)
+
+
+
+  end subroutine testing
+
+
+end module test_array_things_test_suite
+
+program test_array_things
+  use :: test_array_things_test_suite
+  implicit none
+
+  call testing()
+
+end program test_array_things
