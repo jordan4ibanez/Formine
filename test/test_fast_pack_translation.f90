45687e225db9f708b759db2284e0f49e5366e38d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-23 07:39:43 -0400

    clean this MESS up

--- test/test_fast_pack_translation.f90
+++ test/test_fast_pack_translation.f90
@@ -16,52 +16,18 @@ contains
     ! type(texture_packer_conf) :: config
     type(fast_packer) :: packer
     integer :: i
-    character(len = :, kind = c_char), allocatable :: root_path, temp_path
+    character(len = :, kind = c_char), allocatable :: root_path, temp_path, temp_key
 
-    ! integer(1), dimension(:), allocatable :: raw_image_data
-    ! integer(c_int) :: width, height, channels, result
-    ! type(memory_texture) :: rgba_image_data
-
-
-    ! config%max_width = 400
-    ! config%max_height = 400
-    ! config%trim = .false.
-    ! config%allow_rotation = .false.
-    ! config%texture_outlines = .true.
-    ! config%border_padding = 2
-    ! config%force_max_dimensions = .false.
-
-    ! packer = texture_packer(config)
 
     root_path = "./test/textures/"
 
     do i = 1,10
 
-      ! print*,size(packer%packer%skylines)
-
-      ! print*,"----[NEW PACK]----"
       temp_path = root_path//int_to_string(i)//".png"
 
-      call packer%pack(temp_path)
+      temp_key = string_get_file_name(temp_path)
 
-      ! raw_image_data = stbi_load(temp_path, width, height, channels, 4)
-
-      ! print*,"len",size(raw_image_data) / 4
-      ! print*,width, height, channels
-
-      ! rgba_image_data = memory_texture(raw_image_data, width, height)
-
-
-      ! ! print*,packer%packer%skylines
-      ! result = packer%pack(temp_path, rgba_image_data)
-
-      ! if(result == TEXTURE_PACKER_OK) then
-      !   ! print*,"PACKED"
-      !   ! print*,packer%packer%skylines
-      !   ! print*,"skyline size:",size(packer%packer%skylines)
-      ! else
-      !   print*,result, "failed to pack"
-      ! end if
+      call packer%pack(temp_key, temp_path)
     end do
 
   end subroutine
