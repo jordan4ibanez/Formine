bcfc71ff92c19bea6e313f829362cd623f20457c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-25 02:55:50 -0400

    Let's see if this thing leaks memory

--- test/test_fast_pack_translation.f90
+++ test/test_fast_pack_translation.f90
@@ -37,8 +37,45 @@ contains
   end subroutine begin_test
 
 
+  subroutine test_memory_leak()
+    use :: iso_c_binding
+    use :: string
+    use :: stb_image
+    use :: memory_texture_module
+    implicit none
 
-  
+    ! type(texture_packer_conf) :: config
+    type(fast_packer) :: packer
+    integer :: i
+    character(len = :, kind = c_char), allocatable :: root_path, temp_path, temp_key
+    type(fast_packer_config) :: config
+
+    config%padding = 1
+    config%width = 400
+    config%height = 400
+    config%enable_trimming = .true.
+    packer = fast_packer(config)
+
+    root_path = "./test/textures/"
+
+    print*,"begin fast packer test"
+
+    do i = 1,10
+
+      temp_path = root_path//int_to_string(i)//".png"
+
+      print*,temp_path
+
+      temp_key = string_get_file_name(temp_path)
+
+      call packer%pack(temp_key, temp_path)
+    end do
+
+    ! testing = packer%save_to_memory_texture()
+
+    call packer%save_to_png("./test/textures/packer_test_result.png")
+  end subroutine test_memory_leak
+
 
 
 end module test_fast_pack_suite
