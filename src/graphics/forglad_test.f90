3ecb74f58ea5c9c7d1419c82aa1d247335f70f92
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-29 08:32:38 -0400

    Now disable everything

--- src/graphics/forglad_test.f90
+++ src/graphics/forglad_test.f90
@@ -3,16 +3,16 @@ module forglad_test
   use, intrinsic :: iso_c_binding
   implicit none
 
-  procedure(gl_clear_c), pointer :: gl_clear
+  ! procedure(gl_clear_c), pointer :: gl_clear
 
   interface
 
-    subroutine gl_clear_c(mask) bind(c)
-      use, intrinsic :: iso_c_binding
-      implicit none
+    ! subroutine gl_clear_c(mask) bind(c)
+    !   use, intrinsic :: iso_c_binding
+    !   implicit none
 
-      integer(c_int), intent(in), value :: mask
-    end subroutine gl_clear_c
+    !   integer(c_int), intent(in), value :: mask
+    ! end subroutine gl_clear_c
 
   end interface
 
@@ -20,17 +20,17 @@ contains
 
 
 
-  subroutine forglad_test_init()
-    use :: string
-    implicit none
+  ! subroutine forglad_test_init()
+  !   use :: string
+  !   implicit none
 
-    type(c_funptr) :: function_pointer
-    character(len = :, kind = c_char), allocatable :: function_name
+  !   type(c_funptr) :: function_pointer
+  !   character(len = :, kind = c_char), allocatable :: function_name
 
-    function_name = into_c_string("glClear")
-    function_pointer = glfw_get_proc_address(function_name)
-    call c_f_procpointer(function_pointer, gl_clear)
+  !   function_name = into_c_string("glClear")
+  !   function_pointer = glfw_get_proc_address(function_name)
+  !   call c_f_procpointer(function_pointer, gl_clear)
 
-  end subroutine forglad_init
+  ! end subroutine forglad_test_init
 
 end module forglad_test
