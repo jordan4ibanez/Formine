61cc7492c6bb17b9931cd8aa993413693ee8c558
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-08-31 13:30:38 -0400

    Create keyboard.f90

---
+++ src/input/keyboard.f90
@@ -0,0 +1,24 @@
+module keyboard
+  use :: glfw
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+
+  private
+
+
+  public :: keyboard_input_callback
+
+
+contains
+
+
+  subroutine keyboard_input_callback()
+    implicit none
+
+
+
+  end subroutine keyboard_input_callback
+
+
+end module keyboard
