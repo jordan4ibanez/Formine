3441a0121e588a70ae7d4a9cd14a2ed3796f4727
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-01 14:24:55 -0400

    Add pr debugging

--- test/test_fast_noise_lite.f90
+++ test/test_fast_noise_lite.f90
@@ -34,12 +34,40 @@ contains
   end subroutine test_perlin
 
 
+  subroutine fast_noise_pr_debug()
+    implicit none
+
+    type(fnl_state) :: noise_state
+    real(c_float), dimension(128,128) :: noise_data
+    integer(c_int) :: x, y
+
+    noise_state = fnl_state()
+
+    do y = 1,128
+      do x = 1,128
+        noise_data(x,y) = fnl_get_noise_2d(noise_state, real(x, c_float), real(y, c_float))
+      end do
+    end do
+  end subroutine fast_noise_pr_debug
 end module test_fast_noise_suite
 
 program test_fast_noise
-  use :: test_fast_noise_suite
+  use :: fast_noise_lite
+  use, intrinsic :: iso_c_binding
   implicit none
 
-  call test_perlin()
+  type(fnl_state) :: noise_state
+  real(c_float), dimension(128,128) :: noise_data
+  integer(c_int) :: x, y
 
+  noise_state = fnl_state()
+  noise_state%noise_type = FNL_NOISE_OPENSIMPLEX2
+
+  do y = 1,128
+    do x = 1,128
+      noise_data(x,y) = fnl_get_noise_3d(noise_state, real(x, c_float), real(y, c_float), 0.0)
+    end do
+  end do
+
+  print*,noise_data
 end program test_fast_noise
