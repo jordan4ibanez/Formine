79d0ed07f2640e52c84b0941943ae049b4d80fe6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-03 12:48:19 -0400

    Begin toying with threads

---
+++ src/bindings/for_p_thread.c
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+#include <pthread.h>
+
---
+++ src/bindings/thread.f90
@@ -0,0 +1,10 @@
+module thread
+  implicit none
+  ! https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
+  ! https://hpc-tutorials.llnl.gov/posix/what_is_a_thread/
+
+  private
+
+  
+
+end module thread
