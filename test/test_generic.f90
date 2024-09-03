619dd05b0ca332eb281f91233de6cf849115b5b8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-03 11:16:29 -0400

    Add a tutorial I made for someone

---
+++ test/test_generic.f90
@@ -0,0 +1,11 @@
+! program bloop
+!   use :: generic_function
+!   implicit none
+
+!   character(:), allocatable :: data
+
+!   data = "hi there"
+
+!   call blueprint_handler(hi_implementation, data)
+
+! end program bloop
