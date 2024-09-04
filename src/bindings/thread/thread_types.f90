f4396a0f74ae3c5d8831af8308b1da0aa3a2d231
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-04 07:58:25 -0400

    Fix this

--- src/bindings/thread/thread_types.f90
+++ src/bindings/thread/thread_types.f90
@@ -2,6 +2,7 @@ module thread_types
   use, intrinsic :: iso_c_binding
   implicit none
 
+
   ! https://ffmpeg.org/doxygen/3.1/os2threads_8h_source.html
   type, bind(c) :: pthread_t
     integer(c_int64_t) :: tid
@@ -9,5 +10,6 @@ module thread_types
     type(c_ptr) :: arg
     type(c_ptr) :: result
   end type pthread_t
+
 
 end module thread_types
