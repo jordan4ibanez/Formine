986a0e3e06d007225b2d6dd034a9affc3549263f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-04 07:04:34 -0400

    Sniff out some error codes

--- src/bindings/thread/for_p_thread.c
+++ src/bindings/thread/for_p_thread.c
@@ -1,3 +1,7 @@
-#include <stdlib.h>
 #include <pthread.h>
+// This one is for looking up error IDs.
+// #include <errno.h>
+// Go through to <errno-base.h>
 
+// You can ctrl - click this to get there.
+// int test = ENOENT;
