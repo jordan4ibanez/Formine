b526a6b8bb9982bb84b4f46284f6bc17e44fd717
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-04 08:00:50 -0400

    Bake this into the executable

--- src/bindings/thread/for_p_thread.c
+++ src/bindings/thread/for_p_thread.c
@@ -6,8 +6,10 @@
 // You can ctrl - click this to get there.
 // int test = ENOENT;
 
+const static int pthread_attr_t_size = sizeof(pthread_attr_t);
+
 //* Make this portable.
 int for_p_thread_get_pthread_attr_t_width()
 {
-  return sizeof(pthread_attr_t);
+  return pthread_attr_t_size;
 }
