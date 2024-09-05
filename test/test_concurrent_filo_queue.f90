da432c1cd77d6d67cc74152aeeac48329865972c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-05 12:16:18 -0400

    push testing framework

--- test/test_concurrent_filo_queue.f90
+++ test/test_concurrent_filo_queue.f90
@@ -1,15 +1,36 @@
 module test_suite_concurrent_filo_queue
-  use :: thread_lifo_queue
+  use :: thread_filo_queue
+  use, intrinsic :: iso_c_binding
   implicit none
 
 
 contains
 
 
-  subroutine test_it()
+  subroutine test_it_single_threaded()
     implicit none
 
-  end subroutine test_it
+    type(concurrent_linked_filo_queue) :: queue
+    integer(c_int) :: i
+    ! integer(c_int), pointer :: i_pointer
+    character(len = :, kind = c_char), pointer :: s_pointer
+
+    queue = concurrent_linked_filo_queue()
+
+    do i = 1,10
+
+      ! allocate(i_pointer)
+      ! i_pointer = i
+      ! call queue%push(i_pointer)
+
+      ! allocate(character(12) :: s_pointer)
+      ! s_pointer = "hi there!"
+
+      ! call queue%push(s_pointer)
+
+
+    end do
+  end subroutine test_it_single_threaded
 
 
 end module test_suite_concurrent_filo_queue
@@ -18,5 +39,5 @@ program test_concurrent_filo_queue
   use :: test_suite_concurrent_filo_queue
   implicit none
 
-  call test_it()
+  call test_it_single_threaded()
 end program test_concurrent_filo_queue
