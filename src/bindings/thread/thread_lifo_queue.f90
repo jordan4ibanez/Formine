91a8ac4f9393ca5e8324462766ed89f901831579
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-05 11:11:41 -0400

    Fill out some basics

--- src/bindings/thread/thread_lifo_queue.f90
+++ src/bindings/thread/thread_lifo_queue.f90
@@ -13,11 +13,40 @@ module thread_lifo_queue
 
 
   type :: concurrent_linked_filo_queue
-
+    type(queue_node), pointer :: head => null()
+    type(queue_node), pointer :: tail => null()
+    type(mutex_rwlock), pointer :: mutex => null()
+  contains
+    procedure :: insert => concurrent_linked_filo_queue_insert
+    procedure :: pop => concurrent_linked_filo_queue_pop
   end type concurrent_linked_filo_queue
 
+  interface concurrent_linked_filo_queue
+    module procedure :: constructor_concurrent_linked_filo_queue
+  end interface concurrent_linked_filo_queue
 
 contains
+
+
+  function constructor_concurrent_linked_filo_queue() result(queue_new)
+    implicit none
+
+    type(concurrent_linked_filo_queue) :: queue_new
+  end function constructor_concurrent_linked_filo_queue
+
+
+  subroutine concurrent_linked_filo_queue_insert(this)
+    implicit none
+
+    class(concurrent_linked_filo_queue), intent(inout) :: this
+  end subroutine concurrent_linked_filo_queue_insert
+
+
+  subroutine concurrent_linked_filo_queue_pop(this)
+    implicit none
+
+    class(concurrent_linked_filo_queue), intent(inout) :: this
+  end subroutine concurrent_linked_filo_queue_pop
 
 
 end module thread_lifo_queue
