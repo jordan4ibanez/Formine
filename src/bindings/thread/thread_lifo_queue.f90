d581920712c64a4ca202938092cc9e83fe2b9e25
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-05 10:58:02 -0400

    Create thread_lifo_queue.f90

---
+++ src/bindings/thread/thread_lifo_queue.f90
@@ -0,0 +1,23 @@
+module thread_lifo_queue
+  use :: thread_types
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+
+  private
+
+
+  type :: queue_node
+
+  end type queue_node
+
+
+  type :: concurrent_linked_filo_queue
+
+  end type concurrent_linked_filo_queue
+
+
+contains
+
+
+end module thread_lifo_queue
