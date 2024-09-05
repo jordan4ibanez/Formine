94746cc6a2d68a223952c9cad8bf311d3b25cc0c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-05 15:30:10 -0400

    Yep, can't really think of any other way to test this

--- test/test_concurrent_filo_queue.f90
+++ test/test_concurrent_filo_queue.f90
@@ -17,46 +17,34 @@ contains
     character(len = :, kind = c_char), allocatable :: stringy
     ! type(queue_data), pointer :: testing
     class(*), pointer :: test_generic_pointer_option
+    logical :: pop_it
 
     queue = concurrent_linked_filo_queue()
 
     print*,"begin test memory leak"
 
-    y = 1
+    pop_it = .false.
 
-    do
+    do y = 1,10000
 
-      ! print*,"iteration: ",y
-
-      y = y + 1
-
-      do i = 1,10
-
-        ! allocate(i_pointer)
-        ! i_pointer = i
-        ! call queue%push(queue_data(i_pointer))
-
+      ! This pushes ~4.5 GB into the queue.
+      do i = 1,200
         stringy = "hi there!"
-
         call queue%push(queue_data(stringy))
       end do
 
-      i = 1
-      do while(queue%pop(test_generic_pointer_option))
-        ! print*,"pop ", i
+      if (pop_it) then
+        do while(queue%pop(test_generic_pointer_option))
+          deallocate(test_generic_pointer_option)
+        end do
+      else
+        call queue%destroy()
+      end if
 
-        i = i + 1
-
-        deallocate(test_generic_pointer_option)
-
-      end do
-
-      ! call queue%destroy()
-
+      pop_it = .not. pop_it
     end do
 
-
-
+    print*, "end test memory leak"
   end subroutine test_it_single_threaded
 
 
