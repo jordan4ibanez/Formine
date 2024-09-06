20700e5dd52c3b16b8dbea7b59bcd5432c9ca8c4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-06 12:12:24 -0400

    Handy dandy spot to put thread runners

---
+++ src/bindings/thread/thread_handler.f90
@@ -0,0 +1,19 @@
+module thread_handler
+  use :: thread
+  implicit none
+
+  !* This is simply an easy way to put all the threading handling in one spot.
+  !* Nothing but main should be using this as an import.
+
+contains
+
+
+  subroutine thread_handler_run()
+    implicit none
+
+    call thread_process_detached_thread_queue()
+
+  end subroutine thread_handler_run
+
+
+end module thread_handler
