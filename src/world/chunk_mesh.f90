077bc35a0761821fb82f7b023bd153845d7dddee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-08 19:50:59 -0400

    It is now broken

--- src/world/chunk_mesh.f90
+++ src/world/chunk_mesh.f90
@@ -116,22 +116,23 @@ contains
     use, intrinsic :: iso_c_binding
     implicit none
 
+    !? Parameters (IN).
     type(c_ptr), intent(in), value :: c_arg_pointer
     type(c_ptr) :: void_pointer
     type(thread_argument), pointer :: arguments
     type(chunk_mesh_generator_message), pointer :: generator_message
-
+    !? Working variables.
     integer(c_int) :: status
-    ! character(len = :, kind = c_char), allocatable :: mesh_id
-    ! type(block_definition), pointer :: definition_pointer
-    ! type(texture_rectangle), pointer :: tr_pointer
+    character(len = :, kind = c_char), allocatable :: mesh_id
+    type(block_definition), pointer :: definition_pointer
+    type(texture_rectangle), pointer :: tr_pointer
     ! Written like this to denote the multiplicative each should have.
-    ! real(c_float), dimension(12), allocatable :: positions(:)
-    ! real(c_float), dimension(8), allocatable :: texture_coordinates(:)
-    ! real(c_float), dimension(12), allocatable :: colors(:)
-    ! integer(c_int), dimension(6), allocatable :: indices(:)
-    ! integer(c_int) :: limit, i, x, z, y, current_offset, p_index, t_index, c_index, i_index, base_y, max_y
-    ! type(vec3i) :: direction, pos, trajectory, offset
+    real(c_float), dimension(12), allocatable :: positions(:)
+    real(c_float), dimension(8), allocatable :: texture_coordinates(:)
+    real(c_float), dimension(12), allocatable :: colors(:)
+    integer(c_int), dimension(6), allocatable :: indices(:)
+    integer(c_int) :: limit, i, x, z, y, current_offset, p_index, t_index, c_index, i_index, base_y, max_y
+    type(vec3i) :: direction, pos, trajectory, offset
 
     !? Transfer main argument pointer to Fortran.
 
@@ -168,125 +169,122 @@ contains
     end if
 
 
+    !? Begin implementation.
 
-    ! fixme: this is EXTREMELY unsafe.
-    ! Need: string and array
+    !! BEGIN EXTREMELY UNSAFE UNTHREADED IMPL EXAMPLE.
     ! texture_atlas_get_texture_rectangle_pointer(definition_pointer%textures(1)%get_pointer())
-
-
-
-    ! ! Very pointy. =>
     ! definition_pointer => block_repo_get_definition_pointer_by_id(1)
+    !! END EXTREMELY UNSAFE UNTHREADED IMPL EXAMPLE.
 
-    ! ! Now we assign.
+    ! Now we assign.
 
-    ! ! Limit it to 1 million faces per chunk.
-    ! ! We will grab a slice of this later to shrink it.
-    ! limit = 200000
+    ! Limit it to 1 million faces per chunk.
+    ! We will grab a slice of this later to shrink it.
+    limit = 200000
 
-    ! allocate(positions(12 * limit))
-    ! allocate(texture_coordinates(8 * limit))
-    ! allocate(colors(12 * limit))
-    ! allocate(indices(6 * limit))
+    allocate(positions(12 * limit))
+    allocate(texture_coordinates(8 * limit))
+    allocate(colors(12 * limit))
+    allocate(indices(6 * limit))
 
-    ! current_offset = 0
+    current_offset = 0
 
-    ! p_index = -1
-    ! t_index = -1
-    ! c_index = -1
-    ! i_index = -1
+    p_index = -1
+    t_index = -1
+    c_index = -1
+    i_index = -1
 
-    ! base_y = (MESH_STACK_HEIGHT * (generator_message%mesh_stack - 1)) + 1
-    ! max_y = MESH_STACK_HEIGHT * (generator_message%mesh_stack)
+    base_y = (MESH_STACK_HEIGHT * (generator_message%mesh_stack - 1)) + 1
+    max_y = MESH_STACK_HEIGHT * (generator_message%mesh_stack)
 
-    ! do x = 1,CHUNK_WIDTH
-    !   do z = 1,CHUNK_WIDTH
-    !     do y = base_y,max_y
+    do x = 1,CHUNK_WIDTH
+      do z = 1,CHUNK_WIDTH
+        do y = base_y,max_y
 
-    !       ! Position in indices.
-    !       pos = [x, y, z]
+          ! Position in indices.
+          pos = [x, y, z]
 
-    !       ! Cycle on air.
-    !       if (generator_message%current%data(pos%y, pos%z, pos%x)%id == 0) then
-    !         cycle
-    !       end if
+          ! Cycle on air.
+          if (generator_message%current%data(pos%y, pos%z, pos%x)%id == 0) then
+            cycle
+          end if
 
-    !       ! Position in world offset.
-    !       offset = [ &
-    !         x - 1, &
-    !         y - base_y, &
-    !         z - 1 &
-    !         ]
+          ! Position in world offset.
+          offset = [ &
+            x - 1, &
+            y - base_y, &
+            z - 1 &
+            ]
 
-    !       do i = 1,6
+          do i = 1,6
 
-    !         ! Direction we're looking towards.
-    !         direction = DIRECTIONS(1:3, i)
+            ! Direction we're looking towards.
+            direction = DIRECTIONS(1:3, i)
 
-    !         ! Block we're looking at.
-    !         trajectory = pos + direction
+            ! Block we're looking at.
+            trajectory = pos + direction
 
-    !         ! If we're going to go out of bounds, cycle.
-    !         ! todo: check neighbor.
-    !         if (trajectory%x < 1 .or. trajectory%x > CHUNK_WIDTH .or. trajectory%z < 1 .or. trajectory%z > CHUNK_WIDTH .or. trajectory%y < 1 .or. trajectory%y > CHUNK_HEIGHT) then
-    !           cycle
-    !         end if
+            ! If we're going to go out of bounds, cycle.
+            ! todo: check neighbor.
+            if (trajectory%x < 1 .or. trajectory%x > CHUNK_WIDTH .or. trajectory%z < 1 .or. trajectory%z > CHUNK_WIDTH .or. trajectory%y < 1 .or. trajectory%y > CHUNK_HEIGHT) then
+              cycle
+            end if
 
-    !         ! todo: if y height == chunk_height then just render the face.
+            ! todo: if y height == chunk_height then just render the face.
 
-    !         ! If it's another fullsize block, cycle.
-    !         ! todo: check draw_type.
-    !         if (generator_message%current%data(trajectory%y, trajectory%z, trajectory%x)%id /= 0) then
-    !           cycle
-    !         end if
+            ! If it's another fullsize block, cycle.
+            ! todo: check draw_type.
+            if (generator_message%current%data(trajectory%y, trajectory%z, trajectory%x)%id /= 0) then
+              cycle
+            end if
 
 
-    !         p_index = (current_offset * 12) + 1
-    !         positions(p_index:p_index + 11) = (FACES(1:12, i) + (/offset%x, offset%y, offset%z, offset%x, offset%y, offset%z, offset%x, offset%y, offset%z, offset%x, offset%y, offset%z/))
+            p_index = (current_offset * 12) + 1
+            positions(p_index:p_index + 11) = (FACES(1:12, i) + (/offset%x, offset%y, offset%z, offset%x, offset%y, offset%z, offset%x, offset%y, offset%z, offset%x, offset%y, offset%z/))
 
-    !         tr_pointer => texture_atlas_get_texture_rectangle_pointer(definition_pointer%textures(1)%get_pointer())
+            tr_pointer => texture_atlas_get_texture_rectangle_pointer(definition_pointer%textures(1)%get_pointer())
 
-    !         t_index = (current_offset * 8) + 1
-    !         texture_coordinates(t_index:t_index + 7) = (/ &
-    !           tr_pointer%min_x,tr_pointer%min_y, &
-    !           tr_pointer%min_x,tr_pointer%max_y, &
-    !           tr_pointer%max_x,tr_pointer%max_y, &
-    !           tr_pointer%max_x,tr_pointer%min_y &
-    !           /)
+            t_index = (current_offset * 8) + 1
+            texture_coordinates(t_index:t_index + 7) = (/ &
+              tr_pointer%min_x,tr_pointer%min_y, &
+              tr_pointer%min_x,tr_pointer%max_y, &
+              tr_pointer%max_x,tr_pointer%max_y, &
+              tr_pointer%max_x,tr_pointer%min_y &
+              /)
 
-    !         c_index = (current_offset * 12) + 1
-    !         colors(c_index:c_index + 11) = (/&
-    !           1.0, 1.0, 1.0, &
-    !           1.0, 1.0, 1.0, &
-    !           1.0, 1.0, 1.0, &
-    !           1.0, 1.0, 1.0 &
-    !           /)
+            c_index = (current_offset * 12) + 1
+            colors(c_index:c_index + 11) = (/&
+              1.0, 1.0, 1.0, &
+              1.0, 1.0, 1.0, &
+              1.0, 1.0, 1.0, &
+              1.0, 1.0, 1.0 &
+              /)
 
-    !         ! ! I love Fortran's array intrinsics.
-    !         i_index = (current_offset * 6) + 1
-    !         indices(i_index:i_index + 5) = (BASE_INDICES + (4 * current_offset))
+            ! ! I love Fortran's array intrinsics.
+            i_index = (current_offset * 6) + 1
+            indices(i_index:i_index + 5) = (BASE_INDICES + (4 * current_offset))
 
-    !         current_offset = current_offset + 1
-    !       end do
-    !     end do
-    !   end do
-    ! end do
+            current_offset = current_offset + 1
+          end do
+        end do
+      end do
+    end do
 
-    ! ! It's a blank mesh.
-    ! if (p_index == -1) then
-    !   mesh_id = ""
-    !   return
-    ! end if
+    ! It's a blank mesh.
+    if (p_index == -1) then
+      mesh_id = ""
+      return
+    end if
 
-    ! p_index = p_index + 11
-    ! t_index = t_index + 7
-    ! c_index = c_index + 11
-    ! i_index = i_index + 5
+    p_index = p_index + 11
+    t_index = t_index + 7
+    c_index = c_index + 11
+    i_index = i_index + 5
 
-    ! positions = positions(1: p_index)
-    ! texture_coordinates = texture_coordinates(1: t_index)
-    ! colors = colors(1: c_index)
-    ! indices = indices(1:i_index)
+    positions = positions(1: p_index)
+    texture_coordinates = texture_coordinates(1: t_index)
+    colors = colors(1: c_index)
+    indices = indices(1:i_index)
 
 
     !! fixme: this should be passing it back into a concurrent FILO queue.
