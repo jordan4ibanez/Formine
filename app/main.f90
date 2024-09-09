49bc776dd7e903db5294cae473be724a6e11b745
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-08 20:52:06 -0400

    this wasn't imported??

--- app/main.f90
+++ app/main.f90
@@ -72,8 +72,6 @@ program main
   ! Get portable function pointers.
   call forglad_load_gl(c_funloc(glfw_get_proc_address))
 
-  ! Only initialize thread when we're sure the game starts up.
-  call thread_initialize()
 
   call mouse_initialize()
 
