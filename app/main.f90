0eacf0a8df19e9422f8864c14799f86063e4fa22
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-09 21:59:30 -0400

    Update main.f90

--- app/main.f90
+++ app/main.f90
@@ -147,11 +147,11 @@ program main
 
       ! do x = -5,5
       ! do y = -5,5
-      do i = 1,3
+      ! do i = 1,3
         call chunk_generator_new_chunk(x, 1)
         call chunk_handler_delete_chunk(x, 1)
         x = x + 1
-      end do
+      ! end do
       !   end do
       ! end do
 
