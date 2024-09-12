90f3a84c90d538fac196ebe6ee01b7320b46140f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-11 21:53:24 -0400

    whoops

--- src/bindings/thread/for_p_thread.c
+++ src/bindings/thread/for_p_thread.c
@@ -22,11 +22,5 @@ int for_p_thread_get_pthread_mutex_t_width()
 //* You can thank tavianator: https://www.reddit.com/r/C_Programming/comments/6zxnr1/comment/dmzuwt6
 int for_p_thread_get_cpu_threads()
 {
-  int thread_count = sysconf(_SC_NPROCESSORS_ONLN);
-  thread_count = thread_count - 1;
-  if (thread_count == 0)
-  {
-    thread_count = 1;
-  }
-  return thread_count;
+  return sysconf(_SC_NPROCESSORS_ONLN);
 }
