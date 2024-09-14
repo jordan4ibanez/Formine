93b0b458a24a3dc908ee6f62d29e48e0a32371c1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-14 02:36:50 -0400

    a very smol oversight

--- src/world/chunk_handler.f90
+++ src/world/chunk_handler.f90
@@ -191,7 +191,7 @@ contains
       do i = 1,MESH_STACK_ARRAY_SIZE
         current_mesh_id => chunk%mesh(i)%get_pointer()
 
-        if (current_mesh_id == "") then
+        if (.not. associated(current_mesh_id) .or. current_mesh_id == "") then
           cycle
         end if
 
