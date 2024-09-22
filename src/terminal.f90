86fda3a4fec39bd15aee1e1c2a160785ab30cf17
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-22 11:17:00 -0400

    make this real nice

--- src/terminal.f90
+++ src/terminal.f90
@@ -8,38 +8,59 @@ module terminal
   public :: colorize_term
 
 
+  integer(c_int), parameter, public :: ERROR = 4
+  integer(c_int), parameter, public :: WARNING = 3
+  integer(c_int), parameter, public :: ADVISORY = 2
+  integer(c_int), parameter, public :: NOTIFICATION = 1
+
+
   ! The 4 seasons, because I think that's neat.
 
   ! Winter red. Christmas ornament.
-  character(9), parameter :: ERROR_TEXT = "176;27;46"
+  character(9), parameter :: ERROR_COLOR = "176;27;46"
 
   ! Autumn orange. Pumpkin.
-  character(10), parameter :: WARNING_TEXT = "255;117;24"
+  character(10), parameter :: WARNING_COLOR = "255;117;24"
 
   ! Summer yellow. Sunflower.
-  character(10), parameter :: ADVISORY_TEXT = "232;222;42"
+  character(10), parameter :: ADVISORY_COLOR = "232;222;42"
 
   ! Spring blue. Easter egg.
-  character(11), parameter :: NOTIFICATION_TEXT = "118;236;251"
+  character(11), parameter :: NOTIFICATION_COLOR = "118;236;251"
 
 
 contains
 
 
-  !* Colorize a string which when output to ANSI terminals will print in color. Yay!
-  !* Utilizes an RGB string. Use to_rgb_string() for an easy way to get this.
-  function colorize_term(input_string, rgb_string) result(colorized_text)
+  !* Colorize a string which when output to ANSI terminals will print in color.
+  !*
+  !* Severity levels: ERROR, WARNING, ADVISORY, NOTIFICATION
+  !*
+  !* If you put in anything else for severity, it will not colorize it.
+  function colorize_term(input_string, severity_level) result(colorized_text)
     implicit none
 
     character(len = *, kind = c_char), intent(in) :: input_string
-    character(len = *, kind = c_char), intent(in) :: rgb_string
+    integer(c_int), intent(in), value :: severity_level
     character(len = :), allocatable :: colorized_text
 
     ! Using 24 bit color standard.
     ! This might break on REALLY old terminals.
     ! Use semicolons only.
     ! Reference: https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
-    colorized_text = achar(27)//"[38;2;"//rgb_string//"m"//input_string//achar(27)//"[m"
+    select case(severity_level)
+     case (ERROR)
+      colorized_text = achar(27)//"[38;2;"//ERROR_COLOR//"m"//input_string//achar(27)//"[m"
+     case (WARNING)
+      colorized_text = achar(27)//"[38;2;"//WARNING_COLOR//"m"//input_string//achar(27)//"[m"
+     case (ADVISORY)
+      colorized_text = achar(27)//"[38;2;"//ADVISORY_COLOR//"m"//input_string//achar(27)//"[m"
+     case (NOTIFICATION)
+      colorized_text = achar(27)//"[38;2;"//NOTIFICATION_COLOR//"m"//input_string//achar(27)//"[m"
+     case default
+      !? Nothing happens if you try to mess with it.
+      colorized_text = input_string
+    end select
   end function colorize_term
 
 
