6f1b6a812ff6f235d505c08c9582ca8fa3b61373
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-22 09:56:09 -0400

    simplify this mess

--- src/graphics/texture.f90
+++ src/graphics/texture.f90
@@ -24,9 +24,6 @@ module texture
   logical :: debug_mode = .false.
 
 
-  !! FIXME: THIS CAN USE A GETTER TO SIMPLIFY GETTING THE INTEGER FROM THE DATABASE!
-
-
 contains
 
   !* Initialize the module.
@@ -49,7 +46,6 @@ contains
     character(len = *, kind = c_char), intent(in) :: texture_file_path
     integer(c_int) :: x, y, channels, desired_channels
     integer(1), dimension(:), allocatable :: image_data
-    integer(c_int) :: texture_id
     character(len = :, kind = c_char), allocatable :: file_name
 
     ! We always want 4 channels.
@@ -61,53 +57,10 @@ contains
       error stop "[Texture] Error: Could not load texture. File specified from file path ["//texture_file_path//"] does not exist."
     end if
 
-    ! First we must generate the texture ID.
-    texture_id = gl_gen_textures()
-
-    if (texture_id == 0) then
-      error stop "[Texture] Error: Failed to generate OpenGL texture."
-    end if
-
-    ! Bind to it.
-    call gl_bind_texture(GL_TEXTURE_2D, texture_id)
-
-    ! Enable texture clamping to edge.
-    call gl_tex_parameter_i(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
-    call gl_tex_parameter_i(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
-
-    ! Create a completely transparent border color.
-    call gl_tex_parameter_fv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, [0.0, 0.0, 0.0, 0.0]);
-
-    ! Nearest neighbor texture filtering.
-    call gl_tex_parameter_i(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    call gl_tex_parameter_i(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-
-    ! We're telling OpenGL that the width of each data component is 1 byte.
-    call gl_pixel_store_i(GL_UNPACK_ALIGNMENT, 1)
-
-    ! And now, we upload it.
-    call gl_tex_image_2d(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
-
     ! This is done by the file name, we don't care about the path.
     file_name = string_get_file_name(texture_file_path)
 
-    ! We ensure that this thing exists.
-    if (.not. gl_is_texture(texture_id)) then
-      error stop "[Texture] Error: Failed to create texture ["//texture_file_path//"]. Does not exist."
-    else
-      if (debug_mode) then
-        print"(A)", "[Texture]: Created ["//file_name//"] at ID ["//int_to_string(texture_id)//"]"
-      end if
-    end if
-
-    ! Generate the mipmaps.
-    call gl_generate_mipmap(GL_TEXTURE_2D)
-
-    ! Finally, unbind. Done.
-    call gl_bind_texture(GL_TEXTURE_2D, 0)
-
-    ! Now we can assign it into the database by the file name and dimensions.
-    call set_texture(file_name, texture_id, x, y)
+    call texture_create_from_memory(file_name, image_data, x, y)
   end subroutine texture_create
 
 
