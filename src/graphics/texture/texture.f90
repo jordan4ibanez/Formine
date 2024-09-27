05c3d9f8d5bedc624928d7b7fd8cff6e5ff512ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-27 01:24:19 -0400

    noice

--- src/graphics/texture/texture.f90
+++ src/graphics/texture/texture.f90
@@ -19,7 +19,9 @@ module texture
   public :: texture_clear_database
 
 
+  !* Type: integer(c_int)
   type(hashmap_string_key) :: texture_database
+  !* Type: vec2i
   type(hashmap_string_key) :: texture_size_database
 
 
@@ -34,6 +36,7 @@ contains
     implicit none
 
     texture_database = new_hashmap_string_key(texture_database_gc)
+
     texture_size_database = new_hashmap_string_key(texture_size_database_gc)
   end subroutine texture_module_initialize
 
