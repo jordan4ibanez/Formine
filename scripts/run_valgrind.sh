9efb94569cec3418037ffd8075f4b064de0c79cf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-03 09:57:41 -0400

    Tune that back,  just a bit

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -11,11 +11,9 @@ fi
 	
 
 valgrind --leak-check=full \
-        --show-leak-kinds=all \
-        --track-origins=yes \
-        --verbose \
-        --fair-sched=yes \
-        $location
+         --track-origins=yes \
+         --fair-sched=yes \
+         $location
 
 
 exit 0
