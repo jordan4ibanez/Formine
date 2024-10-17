9a942e94365e8ea13c48bad503e1efe92d3714c5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-17 02:29:49 -0400

    A small gameplan

--- src/world/chunk_update_controller.f90
+++ src/world/chunk_update_controller.f90
@@ -9,4 +9,18 @@ module chunk_update_controller
 contains
 
 
+  subroutine chunk_update_controller_new_chunk(x,z)
+    implicit none
+
+    integer(c_int), intent(in), value :: x,z
+
+    ! todo: talk to the chunk generator.
+
+  end subroutine chunk_update_controller_new_chunk
+
+
+  ! todo: hashset queue
+
+  ! todo: single block update which calculates into the specific portion of the chunk
+
 end module chunk_update_controller
